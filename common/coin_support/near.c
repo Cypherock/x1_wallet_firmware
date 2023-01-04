164485be07896323038db95688f8f32e24fafa04
Tejasv Sharma <tejasvonly@gmail.com>
2023-01-04 20:51:00 +0530

    undo unrelated changes

--- common/coin_support/near.c
+++ common/coin_support/near.c
@@ -58,28 +58,29 @@
 #include "near.h"
 
 void near_byte_array_to_unsigned_txn(uint8_t *byte_array, uint16_t byte_array_size, near_unsigned_txn *utxn) {
-  uint16_t offset        = 0;
-  utxn->signer_id_length = U32_READ_LE_ARRAY(byte_array);
-  offset += 4;
-  utxn->signer = (byte_array + offset);
-  offset += utxn->signer_id_length;
-  utxn->signer_key.key_type = byte_array[offset++];
-  utxn->signer_key.key      = (byte_array + offset);
-  offset += 32;
-  memcpy(utxn->nonce, byte_array + offset, sizeof(utxn->nonce));
-  offset += 8;
-  utxn->receiver_id_length = U32_READ_LE_ARRAY(byte_array + offset);
-  offset += 4;
-  utxn->receiver = (byte_array + offset);
-  offset += utxn->receiver_id_length;
-  utxn->blockhash = (byte_array + offset);
-  offset += 32;
-  utxn->action_count = U32_READ_LE_ARRAY(byte_array + offset);
-  offset += 4;
-  utxn->actions_type = byte_array[offset++];
+    uint16_t offset = 0;
+    utxn->signer_id_length = U32_READ_LE_ARRAY(byte_array);
+    offset += 4;
+    utxn->signer = (byte_array + offset);
+    offset += utxn->signer_id_length;
+    utxn->signer_key.key_type = byte_array[offset++];
+    utxn->signer_key.key = (byte_array + offset);
+    offset += 32;
+    memcpy(utxn->nonce, byte_array + offset, sizeof(utxn->nonce));
+    offset += 8;
+    utxn->receiver_id_length = U32_READ_LE_ARRAY(byte_array + offset);
+    offset += 4;
+    utxn->receiver = (byte_array + offset);
+    offset += utxn->receiver_id_length;
+    utxn->blockhash = (byte_array + offset);
+    offset += 32;
+    utxn->action_count = U32_READ_LE_ARRAY(byte_array + offset);
+    offset += 4;
+    utxn->actions_type = byte_array[offset++];
 
-  //assuming action_count as 1
-  switch (utxn->actions_type) {
+    //assuming action_count as 1
+    switch (utxn->actions_type) {
+
     case NEAR_ACTION_TRANSFER:
       memcpy(utxn->action.transfer.amount, byte_array + offset, sizeof(utxn->action.transfer.amount));
       cy_reverse_byte_array(utxn->action.transfer.amount, sizeof(utxn->action.transfer.amount));
@@ -87,101 +88,112 @@ void near_byte_array_to_unsigned_txn(uint8_t *byte_array, uint16_t byte_array_si
 
     case NEAR_ACTION_FUNCTION_CALL:
       utxn->action.fn_call.method_name_length = U32_READ_LE_ARRAY(byte_array + offset);
-      offset += 4;
+      offset+=4;
       utxn->action.fn_call.method_name = (char *)(byte_array + offset);
-      offset += utxn->action.fn_call.method_name_length;
-      utxn->action.fn_call.args_length = U32_READ_LE_ARRAY(byte_array + offset);
-      offset += 4;
+      offset+=utxn->action.fn_call.method_name_length;
+      utxn->action.fn_call.args_length = U32_READ_LE_ARRAY(byte_array+offset);
+      offset+=4;
       utxn->action.fn_call.args = (byte_array + offset);
-      offset += utxn->action.fn_call.args_length;
-      memcpy(utxn->action.fn_call.gas, byte_array + offset, sizeof(utxn->action.fn_call.gas));
-      cy_reverse_byte_array(utxn->action.fn_call.gas, sizeof(utxn->action.fn_call.gas));
-      offset += 8;
+      offset+=utxn->action.fn_call.args_length;
+      memcpy(utxn->action.fn_call.gas,byte_array +offset,sizeof(utxn->action.fn_call.gas));
+      cy_reverse_byte_array(utxn->action.fn_call.gas,sizeof(utxn->action.fn_call.gas));
+      offset+=8;
       memcpy(utxn->action.fn_call.deposit, byte_array + offset, sizeof(utxn->action.fn_call.deposit));
       cy_reverse_byte_array(utxn->action.fn_call.deposit, sizeof(utxn->action.fn_call.deposit));
       break;
 
     default:
       break;
-  }
-  // Reverse byte order
-  cy_reverse_byte_array(utxn->nonce, sizeof(utxn->nonce));
-  ASSERT(offset <= byte_array_size);
+    }
+    // Reverse byte order
+    cy_reverse_byte_array(utxn->nonce, sizeof(utxn->nonce));
+    ASSERT(offset <= byte_array_size);
 }
 
-void near_sig_unsigned_byte_array(const uint8_t *unsigned_txn_byte_array,
-                                  uint64_t unsigned_txn_len,
-                                  const txn_metadata *transaction_metadata,
-                                  const char *mnemonics,
-                                  const char *passphrase,
-                                  uint8_t *sig) {
-  uint8_t digest[32] = {0};
-  uint32_t path[]    = {BYTE_ARRAY_TO_UINT32(transaction_metadata->purpose_index),
-                        BYTE_ARRAY_TO_UINT32(transaction_metadata->coin_index),
-                        BYTE_ARRAY_TO_UINT32(transaction_metadata->account_index),
-                        BYTE_ARRAY_TO_UINT32(transaction_metadata->input[0].chain_index),
-                        BYTE_ARRAY_TO_UINT32(transaction_metadata->input[0].address_index)};
-  uint8_t seed[64]   = {0};
-  HDNode hdnode;
-  mnemonic_to_seed(mnemonics, passphrase, seed, NULL);
-  sha256_Raw(unsigned_txn_byte_array, unsigned_txn_len, digest);
-  derive_hdnode_from_path(path, 5, ED25519_NAME, seed, &hdnode);
-  ed25519_sign(digest, 32, hdnode.private_key, hdnode.public_key + 1, sig);
-  memzero(digest, sizeof(digest));
-  memzero(path, sizeof(path));
-  memzero(seed, sizeof(seed));
-  memzero(&hdnode, sizeof(hdnode));
+void near_sig_unsigned_byte_array(const uint8_t *unsigned_txn_byte_array, uint64_t unsigned_txn_len,
+                                 const txn_metadata *transaction_metadata, const char *mnemonics,
+                                 const char *passphrase, uint8_t *sig)
+{
+    uint8_t digest[32]={0};
+    uint32_t path[] = {
+      BYTE_ARRAY_TO_UINT32(transaction_metadata->purpose_index),
+      BYTE_ARRAY_TO_UINT32(transaction_metadata->coin_index),
+      BYTE_ARRAY_TO_UINT32(transaction_metadata->account_index),
+      BYTE_ARRAY_TO_UINT32(transaction_metadata->input[0].chain_index),
+      BYTE_ARRAY_TO_UINT32(transaction_metadata->input[0].address_index)
+    };
+    uint8_t seed[64]={0};
+    HDNode hdnode;
+    mnemonic_to_seed(mnemonics, passphrase, seed,NULL);
+    sha256_Raw(unsigned_txn_byte_array, unsigned_txn_len, digest);
+    derive_hdnode_from_path(path, 5, ED25519_NAME, seed, &hdnode);
+    ed25519_sign(digest, 32, hdnode.private_key, hdnode.public_key+1, sig);
+    memzero(digest, sizeof(digest));
+    memzero(path, sizeof(path));
+    memzero(seed, sizeof(seed));
+    memzero(&hdnode, sizeof(hdnode));
 }
 
-size_t near_get_new_account_id_from_fn_args(const char *args, uint32_t args_len, char *account_id) {
-  const int start = 19;             //length of '{"new_account_id":"'
-  const int end   = args_len - 74;  //length of '","new_public_key":"ed25519:..."}'
+size_t near_get_new_account_id_from_fn_args(const char *args, uint32_t args_len, char *account_id){
+  const int start = 19; //length of '{"new_account_id":"'
+  const int end = args_len - 74; //length of '","new_public_key":"ed25519:..."}'
   memcpy(account_id, args + start, end - start);
-  return end - start;
+  return end-start;
 }
 
-void near_serialize_account_ids(const char **account_ids, const size_t count, uint8_t *data, uint16_t *data_len) {
-  *data_len           = 0;
-  data[(*data_len)++] = TAG_NEAR_DEFAULT_NETWORK;
-  *data_len += 2;  // Leave 2 bytes for storing data length
-  for (size_t i = 0; i < count; i++) {
-    fill_flash_tlv(data, data_len, TAG_NEAR_REGISTERED_ACC, strnlen(account_ids[i], NEAR_ACC_ID_MAX_LEN) + 1,
-                   (const uint8_t *)account_ids[i]);
-  }
-  data[1] = (*data_len - 3);
-  data[2] = (*data_len - 3) >> 8;
+void near_serialize_account_ids(const char ** account_ids,const size_t count,uint8_t* data,uint16_t *data_len){
+    *data_len=0;
+    data[(*data_len)++]=TAG_NEAR_DEFAULT_NETWORK;
+    *data_len += 2;    // Leave 2 bytes for storing data length
+    for(size_t i=0;i<count; i++){
+        fill_flash_tlv(data,data_len,TAG_NEAR_REGISTERED_ACC,strnlen(account_ids[i], NEAR_ACC_ID_MAX_LEN)+1,(const uint8_t *)account_ids[i]);
+    }
+    data[1]=(*data_len-3);
+    data[2]=(*data_len-3) >> 8;
 }
 
-void near_deserialize_account_ids(const uint8_t *data, const uint16_t data_len, char **account_ids, size_t count) {
+void near_deserialize_account_ids(const uint8_t *data,const uint16_t data_len, char **account_ids,size_t count){
   uint16_t offset = 3;
 
   for (size_t i = 0; i < count; i++) {
-    if (data[offset++] != TAG_NEAR_REGISTERED_ACC)
-      return;
+
+    if(data[offset++]!=TAG_NEAR_REGISTERED_ACC) return;
 
     uint16_t acc_id_len = U16_READ_LE_ARRAY(data + offset);
-    offset += 2;
+    offset+=2;
 
-    account_ids[i] = (char *)(data + offset);
-    offset += acc_id_len;
+    account_ids[i] = (char*) (data+offset);
+    offset+=acc_id_len;
 
-    if (offset > data_len)
-      return;
+    if(offset>data_len) return;
   }
 }
 
-size_t near_get_account_ids_count(const uint8_t *data, const uint16_t data_len) {
+size_t near_get_account_ids_count(const uint8_t* data,const uint16_t data_len){
   uint16_t offset = 3;
-  size_t count    = 0;
-  while (offset < data_len) {
-    if (data[offset++] != TAG_NEAR_REGISTERED_ACC)
-      return count;
+  size_t count = 0;
+  while(offset<data_len){
+
+    if(data[offset++]!=TAG_NEAR_REGISTERED_ACC) return count;
 
     uint16_t acc_id_len = 0;
     memcpy(&acc_id_len, data + offset, sizeof(acc_id_len));
-    offset += 2;
-    offset += acc_id_len;
+    offset+=2;
+    offset+=acc_id_len;
     count++;
   }
   return count;
+}
+    size_t count    = 0;
+    while (offset < data_len) {
+        if (data[offset++] != TAG_NEAR_REGISTERED_ACC)
+            return count;
+
+        uint16_t acc_id_len = 0;
+        memcpy(&acc_id_len, data + offset, sizeof(acc_id_len));
+        offset += 2;
+        offset += acc_id_len;
+        count++;
+    }
+    return count;
 }
