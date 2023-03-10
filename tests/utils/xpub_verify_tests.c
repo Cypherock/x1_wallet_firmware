ba66da22fe3fbc65a09846fdd421e07da828642c
Ujjwal Kumar <ujjwal@cypherock.com>
2023-03-10 16:53:24 +0530

    test: add near test case

--- tests/utils/xpub_verify_tests.c
+++ tests/utils/xpub_verify_tests.c
@@ -7,32 +7,59 @@
 
 const uint32_t paths[][7] = {
     // depth_size, validity, purpose_id, coin_id, account_id, change_id, address_id
+    // test for account 0
     { 5, 1, NON_SEGWIT, NEAR, 0x80000000, 0x80000000, 0x80000000},          // near valid
-    { 5, 1, NON_SEGWIT, NEAR, 0x80000000, 0x80000000, 0x8fffffff},          // near valid
+    // test for account 0xffffffff
+    { 5, 1, NON_SEGWIT, NEAR, 0x80000000, 0x80000000, 0xffffffff},          // near valid
+    // wrong purpose-id
     { 5, 0, NATIVE_SEGWIT, NEAR, 0x80000000, 0x80000000, 0x80000000},       // near invalid
+    // wrong coin id
     { 5, 0, NON_SEGWIT, NEAR - 2, 0x80000000, 0x80000000, 0x80000000},      // near invalid
+    // 1 non-hardened index
     { 5, 0, NON_SEGWIT, NEAR, 0x00000000, 0x80000000, 0x8fffffff},          // near invalid
+    // 1 non-hardened index
+    { 5, 0, NON_SEGWIT, NEAR, 0x80000000, 0x80000000, 0x7fffffff},          // near invalid
+    // 2 non-hardened indices
     { 5, 0, NON_SEGWIT, NEAR, 0x00000000, 0x00000000, 0x8fffffff},          // near invalid
+    // 2 non-hardened indices
     { 5, 0, NON_SEGWIT, NEAR, 0x00000000, 0x80000000, 0x0fffffff},          // near invalid
 
+    // sol-paper account
     { 2, 1, NON_SEGWIT, SOLANA },                                           // sol  valid
+    // sol-ledger account 0
     { 3, 1, NON_SEGWIT, SOLANA, 0x80000000 },                               // sol  valid
-    { 3, 1, NON_SEGWIT, SOLANA, 0x8fffffff },                               // sol  valid
+    // sol-ledger account 0x7fffffff
+    { 3, 1, NON_SEGWIT, SOLANA, 0xffffffff },                               // sol  valid
+    // sol-phantom account 0
     { 4, 1, NON_SEGWIT, SOLANA, 0x80000000, 0x80000000 },                   // sol  valid
+    // sol-phantom account 0x7fffffff
     { 4, 1, NON_SEGWIT, SOLANA, 0x8fffffff, 0x80000000 },                   // sol  valid
+    // sol-paper wrong purpose-id
     { 2, 0, NATIVE_SEGWIT, SOLANA },                                        // sol  invalid
+    // sol-ledger non-hardened account
     { 3, 0, NON_SEGWIT, SOLANA, 0x00000000 },                               // sol  invalid
+    // sol-ledger wrong purpose-id and non-hardened account
     { 3, 0, NATIVE_SEGWIT, SOLANA, 0x00000000 },                            // sol  invalid
+    // sol-phantom wrong purpose-id
     { 4, 0, NATIVE_SEGWIT, SOLANA, 0x80000000, 0x80000001 },                // sol  invalid
+    // sol-phantom 
     { 4, 0, NON_SEGWIT, SOLANA, 0x80000000, 0x80000001 },                   // sol  invalid
     { 5, 0, NON_SEGWIT, SOLANA, 0x80000000, 0x80000000, 0x8fffffff},        // sol  invalid
 
+    // account 0
     { 3, 1, NON_SEGWIT, ETHEREUM, 0x80000000 },                             // eth  valid
-    { 3, 1, NON_SEGWIT, ETHEREUM, 0x8fffffff },                             // eth  valid
-    { 5, 0, NON_SEGWIT, ETHEREUM, 0x80000000, 0x80000000, 0x8fffffff},      // eth  invalid
+    // account 0x7fffffff
+    { 3, 1, NON_SEGWIT, ETHEREUM, 0xffffffff },                             // eth  valid
+    // non-hardened
     { 3, 0, NON_SEGWIT, ETHEREUM, 0x00000000 },                             // eth  invalid
+    // wrong depth for xpub
+    { 5, 0, NON_SEGWIT, ETHEREUM, 0x80000000, 0x80000000, 0xffffffff},      // eth  invalid
+    // wrong purpose-id
     { 3, 0, NATIVE_SEGWIT, ETHEREUM, 0x80000000 },                          // eth  invalid
-    { 5, 0, NATIVE_SEGWIT, ETHEREUM, 0x80000000, 0x80000000, 0x8fffffff},   // eth  invalid
+    // wrong depth and purpose-id
+    { 5, 0, NATIVE_SEGWIT, ETHEREUM, 0x80000000, 0x80000000, 0xffffffff},   // eth  invalid
+    // wrong depth, hardened and purpose-id
+    { 5, 0, NATIVE_SEGWIT, ETHEREUM, 0x00000000, 0x80000000, 0xffffffff},   // eth  invalid
 
     { 3, 1, NON_SEGWIT, BITCOIN, 0x80000000 },                              // btc  valid
     { 3, 1, NATIVE_SEGWIT, BITCOIN, 0x80000000 },                           // btc  valid
