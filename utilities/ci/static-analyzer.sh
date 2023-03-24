f99c95b756bcc4525a7be7747d105a3693f1339b
Ujjwal Kumar <ujjwal@cypherock.com>
2023-03-24 19:01:15 +0530

    feat (ci): Add clang-tidy checks on pull requests

---
+++ utilities/ci/static-analyzer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Uncomment the following line to disable clang-tidy checks
+# exit 0
+
+set -e
+
+if [ $# -lt 1 ]
+then
+  echo "USAGE: $0 <GIT_COMMIT_ID>"
+  echo "This script takes GIT_COMMIT_ID as only parameter"
+  exit 1
+fi
+
+GIT_COMMIT_ID=$1
+CHANGED_FILES=$(git diff --name-only "$GIT_COMMIT_ID"...HEAD)
+
+for file in $CHANGED_FILES
+do
+  [ -e "$file" ] || continue
+  case "$file" in
+
+  common/lvgl/*|common/libraries/atecc/*|common/libraries/crypto/*|common/libraries/nanopb/*) continue ;;
+
+  *.c|*.h|*.cpp|*.hpp)
+    echo "::group::$file"
+    clang-tidy -p build "$file" >> anaylysis.results || true
+    echo "::endgroup::"
+  esac
+done
+
+exit 0
