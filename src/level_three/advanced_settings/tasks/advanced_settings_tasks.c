828fe575b2b54710f5844ecefa0e86149af44ebe
Aman Agarwal <aman@cypherock.com>
2023-04-12 20:35:56 +0530

    refactor(app): Replaced constant offsets for images

--- src/level_three/advanced_settings/tasks/advanced_settings_tasks.c
+++ src/level_three/advanced_settings/tasks/advanced_settings_tasks.c
@@ -55,217 +55,216 @@
  *
  ******************************************************************************
  */
-#include "controller_tap_cards.h"
+#include "board.h"
+#include "constant_texts.h"
 #include "controller_level_four.h"
 #include "controller_main.h"
-#include "board.h"
+#include "controller_tap_cards.h"
+#include "cy_card_hc.h"
+#include "cy_factory_reset.h"
 #include "tasks.h"
+#include "tasks_level_four.h"
+#include "tasks_tap_cards.h"
 #include "ui_confirmation.h"
-#include "constant_texts.h"
-#include "utils.h"
+#include "ui_delay.h"
 #include "ui_instruction.h"
 #include "ui_message.h"
-#include "tasks_tap_cards.h"
-#include "tasks_level_four.h"
-#include "ui_delay.h"
-#include "cy_factory_reset.h"
-#include "cy_card_hc.h"
 #include "ui_multi_instruction.h"
+#include "utils.h"
 
-extern lv_task_t* timeout_task;
+extern lv_task_t *timeout_task;
 
 extern const char *GIT_REV;
 extern const char *GIT_TAG;
 extern const char *GIT_BRANCH;
 
-void level_three_advanced_settings_tasks()
-{
+void level_three_advanced_settings_tasks() {
 #if X1WALLET_MAIN
-    uint8_t valid_wallets = get_valid_wallet_count();
+  uint8_t valid_wallets = get_valid_wallet_count();
 #endif
-    switch (flow_level.level_two) {
+  switch (flow_level.level_two) {
     case LEVEL_THREE_RESET_DEVICE_CONFIRM: {
-        transmit_one_byte_confirm(USER_FIRMWARE_UPGRADE_CHOICE);
-        instruction_scr_init(ui_text_processing, NULL);
-        mark_event_over();
+      transmit_one_byte_confirm(USER_FIRMWARE_UPGRADE_CHOICE);
+      instruction_scr_init(ui_text_processing, NULL);
+      mark_event_over();
     } break;
 
 #if X1WALLET_MAIN
     case LEVEL_THREE_SYNC_CARD_CONFIRM: {
-    #ifndef DEV_BUILD
-        if (valid_wallets != get_wallet_count()) {
-            reset_flow_level();
-            mark_error_screen(ui_text_wallet_partial_fix);
-            break;
-        }
-    #endif
-        if(get_keystore_used_count() < 2){
-            tap_card_take_to_pairing();
-            mark_error_screen(ui_text_error_pair_atleast_2_cards);
-            break;
-        }
-        confirm_scr_init(ui_text_sync_x1card_confirm);
+#ifndef DEV_BUILD
+      if (valid_wallets != get_wallet_count()) {
+        reset_flow_level();
+        mark_error_screen(ui_text_wallet_partial_fix);
+        break;
+      }
+#endif
+      if (get_keystore_used_count() < 2) {
+        tap_card_take_to_pairing();
+        mark_error_screen(ui_text_error_pair_atleast_2_cards);
+        break;
+      }
+      confirm_scr_init(ui_text_sync_x1card_confirm);
     } break;
 
-
     case LEVEL_THREE_ROTATE_SCREEN_CONFIRM: {
-        confirm_scr_init(ui_text_rotate_display_confirm);
+      confirm_scr_init(ui_text_rotate_display_confirm);
     } break;
 
     case LEVEL_THREE_TOGGLE_PASSPHRASE: {
-        if(is_passphrase_disabled()) {
-            confirm_scr_init(ui_text_enable_passphrase_step);
-        }
-        else{
-            confirm_scr_init(ui_text_disable_passphrase_step);
-        }
+      if (is_passphrase_disabled()) {
+        confirm_scr_init(ui_text_enable_passphrase_step);
+      } else {
+        confirm_scr_init(ui_text_disable_passphrase_step);
+      }
     } break;
 
     case LEVEL_THREE_FACTORY_RESET:
-        cyt_factory_reset();
-        break;
+      cyt_factory_reset();
+      break;
 
     case LEVEL_THREE_CARD_HEALTH_CHECK:
-        cyt_card_hc();
-        break;
+      cyt_card_hc();
+      break;
 #endif
 
     case LEVEL_THREE_VIEW_DEVICE_VERSION: {
-        uint32_t blVersion = FW_get_bootloader_version(), fwVersion = get_fwVer();
-        uint16_t fwMajor = (fwVersion >> 24) & 0xFF, fwMinor = (fwVersion >> 16) & 0xFF, fwPatch = fwVersion & 0xFFFF;
-        uint16_t blMajor = (blVersion >> 24) & 0xFF, blMinor = (blVersion >> 16) & 0xFF, blPatch = blVersion & 0xFFFF;
-        char fw_msg[60] = {0}, bl_msg[60] = {0};
-        const char *msg[2] = {fw_msg, bl_msg};
+      uint32_t blVersion = FW_get_bootloader_version(), fwVersion = get_fwVer();
+      uint16_t fwMajor = (fwVersion >> 24) & 0xFF, fwMinor = (fwVersion >> 16) & 0xFF, fwPatch = fwVersion & 0xFFFF;
+      uint16_t blMajor = (blVersion >> 24) & 0xFF, blMinor = (blVersion >> 16) & 0xFF, blPatch = blVersion & 0xFFFF;
+      char fw_msg[60] = {0}, bl_msg[60] = {0};
+      const char *msg[2] = {fw_msg, bl_msg};
 
-        snprintf(fw_msg, sizeof(fw_msg), "Firmware Version\n%d.%d.%d-%s", fwMajor, fwMinor, fwPatch, GIT_REV);
-        snprintf(bl_msg, sizeof(bl_msg), "Bootloader Version\n%d.%d.%d", blMajor, blMinor, blPatch);
-        multi_instruction_init(msg, 2, DELAY_TIME, true);
+      snprintf(fw_msg, sizeof(fw_msg), "Firmware Version\n%d.%d.%d-%s", fwMajor, fwMinor, fwPatch, GIT_REV);
+      snprintf(bl_msg, sizeof(bl_msg), "Bootloader Version\n%d.%d.%d", blMajor, blMinor, blPatch);
+      multi_instruction_init(msg, 2, DELAY_TIME, true);
     } break;
 
     case LEVEL_THREE_VERIFY_CARD: {
 #if X1WALLET_MAIN
-        verify_card_task();
+      verify_card_task();
 #elif X1WALLET_INITIAL
-        initial_verify_card_task();
+      initial_verify_card_task();
 #else
 #error Specify what to build (X1WALLET_INITIAL or X1WALLET_MAIN)
 #endif
     } break;
 
     case LEVEL_THREE_READ_CARD_VERSION: {
-        tasks_read_card_id();
+      tasks_read_card_id();
     } break;
 
     case LEVEL_THREE_REGULATORY_INFO: {
-        instruction_content_t content[7] = {0};
-        LV_IMG_DECLARE(fcc_logo_black_2020);
-        LV_IMG_DECLARE(ce_mark);
-        content[0].img = &fcc_logo_black_2020;
-        content[0].img_x_offset = (128 - fcc_logo_black_2020.header.w) >> 1;
-        content[0].img_y_offset = 1;
-        content[0].text_align = LV_ALIGN_OUT_BOTTOM_MID;
-        content[6].img = &ce_mark;
-        content[6].img_x_offset = (128 - ce_mark.header.w) >> 1;
-        content[6].img_y_offset = 2;
+      instruction_content_t content[7] = {0};
+      LV_IMG_DECLARE(fcc_logo_black_2020);
+      LV_IMG_DECLARE(ce_mark);
+      content[0].img          = &fcc_logo_black_2020;
+      content[0].img_x_offset = (LV_HOR_RES_MAX - fcc_logo_black_2020.header.w) >> 1;
+      content[0].img_y_offset = (LV_VER_RES_MAX - fcc_logo_black_2020.header.h) >> 1;
+      content[0].text_align   = LV_ALIGN_OUT_BOTTOM_MID;
+      content[6].img          = &ce_mark;
+      content[6].img_x_offset = (LV_HOR_RES_MAX - ce_mark.header.w) >> 1;
+      content[6].img_y_offset = (LV_VER_RES_MAX - ce_mark.header.h) >> 1;
 
-        snprintf(content[1].text, sizeof(content[1].text), "This device complies with Part 15 of the FCC Rules. Operation is");
-        snprintf(content[2].text, sizeof(content[2].text), "subject to the following two conditions: (1) this device may not");
-        snprintf(content[3].text, sizeof(content[3].text), "cause harmful interference, and (2) this device must accept any");
-        snprintf(content[4].text, sizeof(content[4].text), "interference received, including interference that");
-        snprintf(content[5].text, sizeof(content[5].text), "may cause undesired operation.");
+      snprintf(content[1].text, sizeof(content[1].text),
+               "This device complies with Part 15 of the FCC Rules. Operation is");
+      snprintf(content[2].text, sizeof(content[2].text),
+               "subject to the following two conditions: (1) this device may not");
+      snprintf(content[3].text, sizeof(content[3].text),
+               "cause harmful interference, and (2) this device must accept any");
+      snprintf(content[4].text, sizeof(content[4].text), "interference received, including interference that");
+      snprintf(content[5].text, sizeof(content[5].text), "may cause undesired operation.");
 
-        multi_instruction_with_image_init(content, 7, DELAY_TIME, true);
-    }break;
+      multi_instruction_with_image_init(content, 7, DELAY_TIME, true);
+    } break;
 
 #if X1WALLET_MAIN
 #ifdef DEV_BUILD
     case LEVEL_THREE_UPDATE_CARD_ID: {
-        tasks_update_card_id();
+      tasks_update_card_id();
     } break;
 
     case LEVEL_THREE_CARD_UPGRADE:
-        card_upgrade_task();
-        break;
+      card_upgrade_task();
+      break;
 
     case LEVEL_THREE_ADJUST_BUZZER:
-        menu_init(&ui_text_options_buzzer_adjust[1], 2, ui_text_options_buzzer_adjust[0], false);
-        break;
+      menu_init(&ui_text_options_buzzer_adjust[1], 2, ui_text_options_buzzer_adjust[0], false);
+      break;
 #endif
 
     case LEVEL_THREE_SYNC_CARD: {
-        tap_a_card_and_sync_task();
+      tap_a_card_and_sync_task();
     } break;
 
     case LEVEL_THREE_SYNC_SELECT_WALLET: {
-        mark_event_over();
+      mark_event_over();
     } break;
 
-    case LEVEL_THREE_SYNC_WALLET_FLOW:{
-        sync_cards_task();
+    case LEVEL_THREE_SYNC_WALLET_FLOW: {
+      sync_cards_task();
     } break;
 
     case LEVEL_THREE_ROTATE_SCREEN: {
-        CY_Reset_Not_Allow(true);
-        ui_rotate();
-        set_display_rotation(get_display_rotation() == LEFT_HAND_VIEW ? RIGHT_HAND_VIEW :
-            LEFT_HAND_VIEW, FLASH_SAVE_NOW);
-        CY_Reset_Not_Allow(false);
-        mark_event_over();
+      CY_Reset_Not_Allow(true);
+      ui_rotate();
+      set_display_rotation(get_display_rotation() == LEFT_HAND_VIEW ? RIGHT_HAND_VIEW : LEFT_HAND_VIEW, FLASH_SAVE_NOW);
+      CY_Reset_Not_Allow(false);
+      mark_event_over();
     } break;
 #endif
 
     case LEVEL_THREE_RESET_DEVICE: {
-        CY_Reset_Not_Allow(true);
-        BSP_DelayMs(500);          // Wait for status pull to desktop (which requests at 200ms)
-        FW_enter_DFU();
-        BSP_reset();
+      CY_Reset_Not_Allow(true);
+      BSP_DelayMs(500);  // Wait for status pull to desktop (which requests at 200ms)
+      FW_enter_DFU();
+      BSP_reset();
     } break;
 
 #ifdef ALLOW_LOG_EXPORT
     case LEVEL_THREE_FETCH_LOGS_INIT: {
-        instruction_scr_init(ui_text_sending_logs, NULL);
-        mark_event_over();
+      instruction_scr_init(ui_text_sending_logs, NULL);
+      mark_event_over();
     } break;
 
     case LEVEL_THREE_FETCH_LOGS_WAIT: {
-        mark_event_over();
+      mark_event_over();
     } break;
 
     case LEVEL_THREE_FETCH_LOGS: {
-        logger_task();
-        mark_event_over();
+      logger_task();
+      mark_event_over();
     } break;
 
     case LEVEL_THREE_FETCH_LOGS_FINISH: {
-        ui_text_slideshow_destructor();
-        delay_scr_init(ui_text_logs_sent, DELAY_TIME);
-        CY_Reset_Not_Allow(true);
+      ui_text_slideshow_destructor();
+      delay_scr_init(ui_text_logs_sent, DELAY_TIME);
+      CY_Reset_Not_Allow(true);
     } break;
 #endif
 
 #if X1WALLET_INITIAL
     case LEVEL_THREE_START_DEVICE_PROVISION: {
-        task_device_provision();
+      task_device_provision();
     } break;
 
     case LEVEL_THREE_START_DEVICE_AUTHENTICATION: {
-        task_device_authentication();
+      task_device_authentication();
     } break;
 #elif X1WALLET_MAIN
     case LEVEL_THREE_PAIR_CARD: {
-        tap_card_pair_card_tasks();
+      tap_card_pair_card_tasks();
     } break;
 
     case LEVEL_THREE_TOGGLE_LOGGING: {
-        if (!is_logging_enabled())
-            confirm_scr_init(ui_text_enable_log_export);
-        else
-            confirm_scr_init(ui_text_disable_log_export);
+      if (!is_logging_enabled())
+        confirm_scr_init(ui_text_enable_log_export);
+      else
+        confirm_scr_init(ui_text_disable_log_export);
     } break;
 #else
 #error Specify what to build (X1WALLET_INITIAL or X1WALLET_MAIN)
 #endif
     default:
-        break;
-    }
+      break;
+  }
 }
