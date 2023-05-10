6ec36c44ad8637ff7fd8ab8f694c592e5a6fff1c
Ujjwal Kumar <ujjwal@cypherock.com>
2023-05-10 12:28:42 +0530

    feat (usb): Verify usb event for validity of app-id

--- common/interfaces/desktop_app_interface/usb_event.c
+++ common/interfaces/desktop_app_interface/usb_event.c
@@ -62,7 +62,9 @@
  *****************************************************************************/
 #include <string.h>
 
+#include "core.pb.h"
 #include "memzero.h"
+#include "pb_decode.h"
 #include "usb_api.h"
 #include "usb_api_priv.h"
 
@@ -78,12 +80,17 @@
  * PRIVATE TYPEDEFS
  *****************************************************************************/
 
+typedef struct usb_core_msg {
+  const uint8_t *buffer;
+  uint16_t size;
+} usb_core_msg_t;
+
 /*****************************************************************************
  * STATIC VARIABLES
  *****************************************************************************/
+
 static usb_event_t usb_event;
-static const uint8_t *request_meta;
-static uint16_t request_meta_size;
+static usb_core_msg_t core_msg;
 
 /*****************************************************************************
  * GLOBAL VARIABLES
@@ -94,21 +101,62 @@ static uint16_t request_meta_size;
  *****************************************************************************/
 
 /**
+ * @brief Validates that the received usb event is valid in the current context
+ * of the application/OS.
+ *
+ * @param evt The captured usb event usb_event_t
+ * @param msg The received encoded context buffer
+ */
+static core_error_type_t validate_msg_context(usb_core_msg_t msg);
+
+/**
+ * @brief Clear the fields of core_msg by setting default values
+ */
+static void clear_msg_context();
+
+/**
  * @brief Reset the members of usb_event_t to decided default state.
  * @details The instance after reset will represent the absence of any usb
  * event as well as the members will be invalid/meaningless content.
  *
  * @param evt Pass the reference to an instance of usb_event_t to reset
  */
-void reset_event_obj(usb_event_t *evt);
+static void reset_event_obj(usb_event_t *evt);
 
 /*****************************************************************************
  * STATIC FUNCTIONS
  *****************************************************************************/
-void reset_event_obj(usb_event_t *evt) {
+static void reset_event_obj(usb_event_t *evt) {
   evt->flag = false;
   evt->msg_size = 0;
   evt->p_msg = NULL;
+}
+
+static void clear_msg_context() {
+  core_msg.size = 0;
+  core_msg.buffer = NULL;
+}
+
+static core_error_type_t validate_msg_context(usb_core_msg_t msg) {
+  core_msg_t core_msg_p = CORE_MSG_INIT_ZERO;
+  pb_istream_t stream = pb_istream_from_buffer(msg.buffer, msg.size);
+
+  // invalid buffer ref, 0 size & decode failure are error situation
+  if (false == pb_decode(&stream, CORE_MSG_FIELDS, &core_msg_p) ||
+      NULL == msg.buffer || 0 == msg.size) {
+    return CORE_ERROR_TYPE_INVALID_MSG;
+  }
+
+  // TODO: verify if the core_msg_p.type.cmd.applet_id is valid one ref PR #235
+  // const cy_app_desc_t *app_desc =
+  // registry_get_app_desc(core_msg_p.type.cmd.applet_id);
+
+  // if (NULL == app_desc) {
+  //   return CORE_ERROR_TYPE_UNKNOWN_APP;
+  // }
+
+  // TODO: verify with core context, if applet is invokable/active
+  return CORE_ERROR_TYPE_NO_ERROR;
 }
 
 /*****************************************************************************
@@ -116,19 +164,21 @@ void reset_event_obj(usb_event_t *evt) {
  *****************************************************************************/
 void usb_clear_event() {
   reset_event_obj(&usb_event);
+  clear_msg_context();
   usb_free_msg_buffer();
   usb_reset_state();
 }
 
-void usb_set_event(const uint16_t proto_msg_size,
-                   const uint8_t *proto_buf,
-                   const uint16_t raw_msg_size,
-                   const uint8_t *raw_msg) {
+void usb_set_event(const uint16_t core_msg_size,
+                   const uint8_t *core_msg_buffer,
+                   const uint16_t app_msg_size,
+                   const uint8_t *app_msg) {
   usb_event.flag = true;
-  usb_event.msg_size = raw_msg_size;
-  usb_event.p_msg = raw_msg;
-  request_meta = proto_buf;
-  request_meta_size = proto_msg_size;
+  usb_event.msg_size = app_msg_size;
+  usb_event.p_msg = app_msg;
+
+  core_msg.buffer = core_msg_buffer;
+  core_msg.size = core_msg_size;
 }
 
 bool usb_get_event(usb_event_t *evt) {
@@ -137,9 +187,17 @@ bool usb_get_event(usb_event_t *evt) {
   }
 
   reset_event_obj(evt);
+
   if (usb_event.flag) {
-    memcpy(evt, &usb_event, sizeof(usb_event_t));
-    usb_set_state_executing();
+    core_error_type_t status = validate_msg_context(core_msg);
+    if (CORE_ERROR_TYPE_NO_ERROR != status) {
+      // now clear event as it is not supposed to reach the app
+      usb_clear_event();
+      // TODO: send an error to host
+    } else {
+      memcpy(evt, &usb_event, sizeof(usb_event_t));
+      usb_set_state_executing();
+    }
   }
   return evt->flag;
 }
