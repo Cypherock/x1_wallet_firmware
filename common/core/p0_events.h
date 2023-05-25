a0f8f0a147dc0c213584c9455bdacf00a56272bc
amanCypherock <102408904+amanCypherock@users.noreply.github.com>
2023-05-25 16:50:01 +0530

    Update common/core/p0_events.h
    Co-authored-by: ujjwal-cyph <95833994+ujjwal-cyph@users.noreply.github.com>

--- common/core/p0_events.h
+++ common/core/p0_events.h
@@ -26,6 +26,7 @@
 /*****************************************************************************
  * MACROS AND DEFINES
  *****************************************************************************/
+// 0xFFFFFFFF ms = 4294967295 / (1000 * 3600 * 24) days ~ 49 days
 #define INIFINITE_WAIT_TIMEOUT 0xFFFFFFFF
 
 #define MAX_INACTIVITY_TIMEOUT                                                 \
