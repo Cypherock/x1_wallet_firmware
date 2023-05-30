a9cd0c121a2ffd066c2496c712d2fd3a515d9a7a
Aman Agarwal <aman@cypherock.com>
2023-05-30 11:54:18 +0530

    chore(nfc): Updated comments according to review comments

--- src/card_operations/card_internal.h
+++ src/card_operations/card_internal.h
@@ -31,7 +31,15 @@
  * GLOBAL FUNCTION PROTOTYPES
  *****************************************************************************/
 /**
- * @brief Initializes the applet on the card.
+ * @brief Initializes the applet on the card and performs necessary operations
+ * for selecting the applet.
+ * @details This API returns success if there is no error in applet
+ * initialization and pairing requirements are ment. When an error is detected,
+ * the API copies the corresponding error type to @ref error_type member of @ref
+ * card_data object and returns it. Member of @ref card_data object @ref
+ * error_message is also set with correspondig string in cases where error_type
+ * is either @ref CARD_OPERATION_RETAP_BY_USER_REQUIRED or @ref
+ * CARD_OPERATION_ABORT_OPERATION.
  *
  * @param card_data Pointer to the data structure containing information about
  * the card operation.
@@ -42,6 +50,11 @@ card_error_type_e card_initialize_applet(card_operation_data_t *card_data);
 
 /**
  * @brief Handles the errors encountered during a card operation.
+ * @details If an error is detected,  the API copies the corresponding error
+ * type to @ref error_type member of @ref card_data object and returns it.
+ * Member of @ref card_data object @ref error_message is also set with
+ * correspondig string in cases where error_type is either
+ * CARD_OPERATION_RETAP_BY_USER_REQUIRED or CARD_OPERATION_ABORT_OPERATION.
  *
  * @param card_data Pointer to the data structure containing information about
  * the card operation.
--- src/card_operations/card_operations.h
+++ src/card_operations/card_operations.h
@@ -3,7 +3,7 @@
  * @author  Cypherock X1 Team
  * @brief   Card operations module
  *          Exports all card APIs to application
- * @copyright Copyright (c) ${YEAR} HODL TECH PTE LTD
+ * @copyright Copyright (c) 2023 HODL TECH PTE LTD
  * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
  * target=_blank>https://mitcc.org/</a>
  */
@@ -24,21 +24,31 @@
  *****************************************************************************/
 typedef enum card_errors_type {
   CARD_OPERATION_SUCCESS = 0,
-  CARD_OPERATION_CARD_REMOVED,
-  CARD_OPERATION_PAIRING_REQUIRED,
-  CARD_OPERATION_LOCKED_WALLET,
-  CARD_OPERATION_INCORRECT_PIN_ENTERED,
-  CARD_OPERATION_P0_ABORT_OCCURED,
-  CARD_OPERATION_P0_TIMEOUT_OCCURED,
-  CARD_OPERATION_RETAP_BY_USER_REQUIRED,
-  CARD_OPERATION_ABORT_OPERATION,
+  CARD_OPERATION_CARD_REMOVED,     /** When card is removed before operation
+                                      completion*/
+  CARD_OPERATION_PAIRING_REQUIRED, /** When card not paired but @ref
+                                      pairing_required is set true */
+  CARD_OPERATION_LOCKED_WALLET,    /** Locked wallet detected during wallet
+                                      operation */
+  CARD_OPERATION_INCORRECT_PIN_ENTERED, /** Incorrect pin entered */
+  CARD_OPERATION_P0_ABORT_OCCURED,      /** Abort command received from host */
+  CARD_OPERATION_P0_TIMEOUT_OCCURED,    /** Timeout occured */
 
-  CARD_OPERATION_DEFAULT_INVALID = 0xFF,
+  /** The API setting error using below enums is required to also set an error
+     message for corresponding user action or error specification */
+  CARD_OPERATION_RETAP_BY_USER_REQUIRED, /** Error that can be resolved with a
+                                            user action involving retapping of a
+                                            card. */
+  CARD_OPERATION_ABORT_OPERATION, /** Error that cannot be resolved by user
+                                     action and operation should be aborted. */
+
+  CARD_OPERATION_DEFAULT_INVALID = 0xFF, /** Default invalid value */
 } card_error_type_e;
 
 typedef struct card_operation_data {
   NFC_connection_data nfc_data;
-  const char *error_message;
+  const char *error_message; /** Error message to be displayed for user action
+                                or error specification */
   card_error_type_e error_type;
 } card_operation_data_t;
 /*****************************************************************************
