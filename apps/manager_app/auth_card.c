52a9a798bf5c9efdcc886205b97524b9eb1de951
amanCypherock <102408904+amanCypherock@users.noreply.github.com>
2023-06-03 10:31:20 +0530

    revert(app): Revert commented onboarding check

--- apps/manager_app/auth_card.c
+++ apps/manager_app/auth_card.c
@@ -549,9 +549,9 @@ void card_auth_handler(manager_query_t *query) {
   ASSERT(NULL != query);
 
   /* Validate if this flow is allowed */
-  // if (false == onboarding_step_allowed(ONBOARDING_CARD_AUTHENTICATION)) {
-  //   // TODO: Reject query
-  // }
+  if (false == onboarding_step_allowed(ONBOARDING_CARD_AUTHENTICATION)) {
+    // TODO: Reject query
+  }
 
   if (MANAGER_AUTH_CARD_REQUEST_INITIATE_TAG !=
       query->auth_card.which_request) {
