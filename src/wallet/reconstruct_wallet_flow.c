274a7ee0839bdf6d0e6d4b0a7ba8c1085aaae1c9
Aman Agarwal <aman@cypherock.com>
2023-07-18 16:58:45 +0530

    fix(core): Revert passphrase stage change

--- src/wallet/reconstruct_wallet_flow.c
+++ src/wallet/reconstruct_wallet_flow.c
@@ -192,8 +192,8 @@ reconstruct_state_e reconstruct_wallet_handler(reconstruct_state_e state,
       ui_scrollable_page(
           ui_text_confirm_passphrase, display, MENU_SCROLL_HORIZONTAL, false);
       memzero(display, sizeof(display));
-      next_state =
-          get_state_on_confirm_scr(PIN_INPUT, PASSPHRASE_INPUT, TIMED_OUT);
+      next_state = get_state_on_confirm_scr(
+          PASSPHRASE_CONFIRM, PASSPHRASE_INPUT, TIMED_OUT);
 
       if (PASSPHRASE_CONFIRM == next_state) {
         snprintf(wallet_credential_data.passphrase,
