21f0bdba4130a4051abad1c7f2e4b7d8ce4885f9
Amneesh Singh <natto@weirdnatto.in>
2023-08-12 11:01:06 +0530

    feat: Implement get_public_keys for evm
    Signed-off-by: Amneesh Singh <natto@weirdnatto.in>

--- apps/evm_family/evm_helpers.h
+++ apps/evm_family/evm_helpers.h
@@ -1,26 +1,33 @@
 /**
- * @file    evm_priv.h
+ * @file    evm_helpers.h
  * @author  Cypherock X1 Team
- * @brief   Support for evm app internal operations
- *          This file is defined to separate EVM's internal use functions,
- * flows, common APIs
+ * @brief   Utilities api definitions for EVM chains
  * @copyright Copyright (c) 2023 HODL TECH PTE LTD
  * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
  * target=_blank>https://mitcc.org/</a>
  */
-#ifndef EVM_PRIV_H
-#define EVM_PRIV_H
+#ifndef EVM_HELPERS_H
+#define EVM_HELPERS_H
 
 /*****************************************************************************
  * INCLUDES
  *****************************************************************************/
 
-#include "events.h"
-#include "evm_api.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
 
 /*****************************************************************************
  * MACROS AND DEFINES
  *****************************************************************************/
+
+#define EVM_DRV_LEGACY_DEPTH 4
+#define EVM_DRV_OTHER_DEPTH 5
+
+#define EVM_PUB_KEY_SIZE 65
+#define EVM_SHORT_PUB_KEY_SIZE 33
+
+#define EVM_CHAIN_NAME "ETH"
 
 /*****************************************************************************
  * TYPEDEFS
@@ -30,19 +37,8 @@
  * EXPORTED VARIABLES
  *****************************************************************************/
 
-extern const evm_config_t *g_evm_app;
-
 /*****************************************************************************
  * GLOBAL FUNCTION PROTOTYPES
  *****************************************************************************/
 
-/**
- * @brief Handler for getting public keys for EVM.
- * @details This flow expects EVM_GET_PUBLIC_KEYS_REQUEST_INITIATE_TAG as
- * initial query, otherwise the flow is aborted
- *
- * @param query Reference to the decoded query struct from the host app
- */
-void evm_get_pub_keys(evm_query_t *query);
-
-#endif /* EVM_PRIV_H */
+#endif /* EVM_HELPERS_H */
