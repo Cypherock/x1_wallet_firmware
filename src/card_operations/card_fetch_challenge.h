9b2e219a6ea5f56dcef78245611aa56db6551409
Aman Agarwal <aman@cypherock.com>
2023-08-16 12:44:05 +0530

    refactor(nfc): Get locked wallet challenge refactored

--- src/card_operations/card_fetch_challenge.h
+++ src/card_operations/card_fetch_challenge.h
@@ -1,30 +1,21 @@
 /**
- * @file    card_operations.h
+ * @file    card_fetch_challenge.h
  * @author  Cypherock X1 Team
- * @brief   Header file exporting card operations
- *
+ * @brief   API for fetching wallet challenge from a card
  * @copyright Copyright (c) 2023 HODL TECH PTE LTD
  * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
  * target=_blank>https://mitcc.org/</a>
  */
-#ifndef CARD_OPERATIONS_H
-#define CARD_OPERATIONS_H
+#ifndef CARD_FETCH_CHALLENGE_H
+#define CARD_FETCH_CHALLENGE_H
 
 /*****************************************************************************
  * INCLUDES
  *****************************************************************************/
-#include "card_delete_share.h"
-#include "card_fetch_challenge.h"
-#include "card_fetch_share.h"
-#include "card_fetch_wallet_list.h"
 #include "card_operation_typedefs.h"
-#include "card_pair.h"
-#include "card_read_verify_share.h"
-#include "card_sign.h"
-#include "card_unlock_wallet.h"
-#include "card_write_share.h"
-#include "check_pairing.h"
-#include "read_card_version.h"
+#include "flash_struct.h"
+#include "stdbool.h"
+#include "stdint.h"
 
 /*****************************************************************************
  * MACROS AND DEFINES
@@ -42,4 +33,18 @@
  * GLOBAL FUNCTION PROTOTYPES
  *****************************************************************************/
 
-#endif /* CARD_OPERATIONS_H */
+/**
+ * @brief Fetches wallet challenge data from a card.
+ * @details This function initializes the applet, retrieves the wallet challenge
+ * from the card. It handles various error cases and returns an appropriate
+ * error code. NOTE: The instruction to tap card is not displayed by the
+ * function as the api might be called as a continuation to error handling of a
+ * separate flow
+ *
+ * @param wallet_name The `wallet_name` parameter is a pointer to a uint8_t
+ * array that represents the name of the wallet.
+ *
+ * @return A card_error_type_e value representing the result of the operation.
+ */
+card_error_type_e card_fetch_challenge(const uint8_t *wallet_name);
+#endif
