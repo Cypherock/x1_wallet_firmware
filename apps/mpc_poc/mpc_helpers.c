a8e85cba97a3922892cfab7775117772d21a8c04
Chirag Madaan <madchirag546@gmail.com>
2023-11-27 03:52:38 +0530

    fix: remove redundant messages in init and exit response

--- apps/mpc_poc/mpc_helpers.c
+++ apps/mpc_poc/mpc_helpers.c
@@ -3,6 +3,7 @@
 #include "ecdsa.h"
 #include "curves.h"
 #include "bip32.h"
+#include "coin_specific_data.h"
 
 #include <stdio.h>
 
@@ -41,9 +42,51 @@ void bytes_to_hex(const uint8_t *data, size_t data_len, char *out, size_t out_le
     }
 }
 
+int fetch_pub_key_from_flash(const uint8_t *wallet_id, uint8_t *pub_key) {
+    uint8_t priv_key[32] = {0};
+
+    if (fetch_priv_key_from_flash(wallet_id, priv_key) != 0) {
+        memzero(priv_key, sizeof(priv_key));
+        return 1;
+    }
+
+    pub_key33_from_priv_key(priv_key, pub_key);
+    memzero(priv_key, sizeof(priv_key));
+    
+    return 0;
+}
+
+int fetch_priv_key_from_flash(const uint8_t *wallet_id, uint8_t *priv_key) {
+    const uint16_t PRIV_KEY_LEN = 32;
+    uint16_t len_read = 0;
+    Coin_Specific_Data_Struct coin_specific_data;
+
+    coin_specific_data.coin_type = MPC_APP;
+    memcpy(coin_specific_data.wallet_id, wallet_id, WALLET_ID_SIZE);
+    coin_specific_data.coin_data = priv_key;
+
+    if (get_coin_data(&coin_specific_data, sizeof(priv_key), &len_read) != 0 || 
+        len_read != PRIV_KEY_LEN) {
+
+        memzero(priv_key, sizeof(priv_key));
+        return 1;
+    }
+
+    return 0;
+}
+
 void priv_key_from_seed(const uint8_t *seed, uint8_t *priv_key) {
+    const size_t SEED_LEN = 64;
+    uint8_t sha256_hash[32];
+
+    Hasher hasher;
+    hasher_Init(&hasher, HASHER_SHA2);  
+
+    hasher_Update(&hasher, seed, SEED_LEN);
+    hasher_Final(&hasher, sha256_hash);
+
     for (int i = 0; i < 32; i++) {
-        priv_key[i] = seed[i];
+        priv_key[i] = sha256_hash[i];
     }
 }
 
--- apps/mpc_poc/mpc_helpers.h
+++ apps/mpc_poc/mpc_helpers.h
@@ -4,12 +4,12 @@
 #include <mpc_poc/core.pb.h>
 
 #define ENTITY_INFO_BUFFER_SIZE 256
-static const char base64_chars[] = 
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 void mpc_sign_message(const uint8_t *message, size_t message_len, uint8_t *sig, const uint8_t *priv_key);
 bool mpc_verify_signature(const uint8_t *message, size_t message_len, const uint8_t *sig, const uint8_t *pub_key);
 void bytes_to_hex(const uint8_t *data, size_t data_len, char *out, size_t out_len);
+int fetch_pub_key_from_flash(const uint8_t *wallet_id, uint8_t *pub_key);
+int fetch_priv_key_from_flash(const uint8_t *wallet_id, uint8_t *priv_key);
 void priv_key_from_seed(const uint8_t *seed, uint8_t *priv_key);
 void pub_key33_from_priv_key(const uint8_t *priv_key, uint8_t *pub_key);
 
