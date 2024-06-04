8488ad17364e769a51431bfedfa0b00e0b83876c
vrockz747 <vrockz747@gmail.com>
2024-06-04 12:49:52 +0530

    feat(app): Tron tokens

---
+++ apps/tron_app/tron_contracts.c
@@ -0,0 +1,256 @@
+/**
+ * @file    tron_contracts.c
+ * @author  Cypherock X1 Team
+ * @brief   Tron chain contract parsing logic
+ * @copyright Copyright (c) 2023 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ *target=_blank>https://mitcc.org/</a>
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * (c) Copyright 2023 by HODL TECH PTE LTD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * "Commons Clause" License Condition v1.0
+ *
+ * The Software is provided to you by the Licensor under the License,
+ * as defined below, subject to the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of
+ * rights under the License will not include, and the License does not
+ * grant to you, the right to Sell the Software.
+ *
+ * For purposes of the foregoing, "Sell" means practicing any or all
+ * of the rights granted to you under the License to provide to third
+ * parties, for a fee or other consideration (including without
+ * limitation fees for hosting or consulting/ support services related
+ * to the Software), a product or service whose value derives, entirely
+ * or substantially, from the functionality of the Software. Any license
+ * notice or attribution required by the License must also include
+ * this Commons Clause License Condition notice.
+ *
+ * Software: All X1Wallet associated files.
+ * License: MIT
+ * Licensor: HODL TECH PTE LTD
+ *
+ ******************************************************************************
+ */
+
+/*****************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+
+#include <google/protobuf/any.pb.h>
+#include <pb_decode.h>
+#include <tron/contract.pb.h>
+#include <tron/sign_txn.pb.h>
+#include <tron/tron.pb.h>
+#include <tron_txn_helpers.h>
+
+#include "base58.h"
+#include "coin_utils.h"
+#include "curves.h"
+#include "ecdsa.h"
+#include "hasher.h"
+#include "secp256k1.h"
+#include "sha2.h"
+#include "status_api.h"
+#include "tron_api.h"
+#include "tron_helpers.h"
+#include "tron_priv.h"
+#include "tron_txn_helpers.h"
+#include "ui_core_confirm.h"
+#include "ui_screens.h"
+#include "wallet_list.h"
+
+/*****************************************************************************
+ * EXTERN VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * PRIVATE MACROS AND DEFINES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * PRIVATE TYPEDEFS
+ *****************************************************************************/
+
+/*****************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * STATIC VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * GLOBAL VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * STATIC FUNCTIONS
+ *****************************************************************************/
+
+static bool transfer_contract_txn(tron_transaction_raw_t *raw_txn,
+                                  tron_transaction_contract_t *contract) {
+  uint8_t to_address[TRON_INITIAL_ADDRESS_LENGTH] = {0};
+  // uint8_t owner_address[TRON_INITIAL_ADDRESS_LENGTH] = {0};
+  int64_t amount = 0;
+  char address[TRON_ACCOUNT_ADDRESS_LENGTH + 1] = {0};
+
+  tron_transfer_contract_t transfer_contract =
+      TRON_TRANSFER_CONTRACT_INIT_DEFAULT;
+  google_protobuf_any_t any = contract->parameter;
+  pb_istream_t stream = pb_istream_from_buffer(any.value.bytes, any.value.size);
+
+  if (!pb_decode(&stream, TRON_TRANSFER_CONTRACT_FIELDS, &transfer_contract)) {
+    tron_send_error(ERROR_COMMON_ERROR_CORRUPT_DATA_TAG,
+                    ERROR_DATA_FLOW_DECODING_FAILED);
+    return false;
+  }
+  memcpy(&amount, &transfer_contract.amount, sizeof(int64_t));
+  memcpy(to_address,
+         (uint8_t *)transfer_contract.to_address,
+         TRON_INITIAL_ADDRESS_LENGTH);
+
+  // verify recipient address;
+  if (!base58_encode_check(to_address,
+                           1 + 20,
+                           HASHER_SHA2D,
+                           address,
+                           TRON_ACCOUNT_ADDRESS_LENGTH + 1)) {
+    tron_send_error(ERROR_COMMON_ERROR_UNKNOWN_ERROR_TAG, 2);
+    return false;
+  }
+
+  if (!core_scroll_page(ui_text_verify_address, address, tron_send_error)) {
+    return false;
+  }
+
+  // verify recipient amount
+  char amount_string[30] = {'\0'};
+  char display[100] = {'\0'};
+
+  snprintf(amount_string, sizeof(amount_string), "%lli", amount);
+  snprintf(display,
+           sizeof(display),
+           UI_TEXT_VERIFY_AMOUNT,
+           amount_string,
+           TRON_LUNIT);
+
+  if (!core_confirmation(display, tron_send_error)) {
+    return false;
+  }
+
+  set_app_flow_status(TRON_SIGN_TXN_STATUS_VERIFY);
+  return true;
+}
+
+static bool trigger_smart_contract_txn(tron_transaction_raw_t *raw_txn,
+                                       tron_transaction_contract_t *contract) {
+  tron_trigger_smart_contract_t trigger_smc_contract =
+      TRON_TRIGGER_SMART_CONTRACT_INIT_DEFAULT;
+  google_protobuf_any_t any = contract->parameter;
+  pb_istream_t stream = pb_istream_from_buffer(any.value.bytes, any.value.size);
+
+  if (!pb_decode(
+          &stream, TRON_TRANSFER_CONTRACT_FIELDS, &trigger_smc_contract)) {
+    tron_send_error(ERROR_COMMON_ERROR_CORRUPT_DATA_TAG,
+                    ERROR_DATA_FLOW_DECODING_FAILED);
+    return false;
+  }
+
+  uint8_t contract_address[TRON_INITIAL_ADDRESS_LENGTH];
+  char address[TRON_ACCOUNT_ADDRESS_LENGTH + 1] = {0};
+  int64_t call_value;
+  int64_t call_token_value;
+  int64_t token_id;
+  uint8_t data[68];
+  memcpy(contract_address,
+         (uint8_t *)trigger_smc_contract.contract_address,
+         TRON_INITIAL_ADDRESS_LENGTH);
+  memcpy(&call_value, &trigger_smc_contract.call_value, sizeof(int64_t));
+  memcpy(&call_token_value,
+         &trigger_smc_contract.call_token_value,
+         sizeof(int64_t));
+  memcpy(&token_id, &trigger_smc_contract.token_id, sizeof(int64_t));
+  // memcpy(data, (uint8_t *)trigger_smc_contract.data, 68);
+
+  // verify recipient address
+  if (!base58_encode_check(contract_address,
+                           1 + 20,
+                           HASHER_SHA2D,
+                           address,
+                           TRON_ACCOUNT_ADDRESS_LENGTH + 1)) {
+    tron_send_error(ERROR_COMMON_ERROR_UNKNOWN_ERROR_TAG, 2);
+    return false;
+  }
+
+  if (!core_scroll_page(ui_text_verify_address, address, tron_send_error)) {
+    return false;
+  }
+
+  // verify recipient amount
+  // if (!core_scroll_page("DATA FIELD", (char *)data, tron_send_error)) {
+  //   return false;
+  // }
+
+  set_app_flow_status(TRON_SIGN_TXN_STATUS_VERIFY);
+  return true;
+}
+
+/*****************************************************************************
+ * GLOBAL FUNCTIONS
+ *****************************************************************************/
+
+bool extract_contract_info(tron_transaction_raw_t *raw_txn) {
+  if (!(raw_txn->contract_count > 0)) {
+    tron_send_error(ERROR_COMMON_ERROR_CORRUPT_DATA_TAG,
+                    ERROR_DATA_FLOW_INVALID_DATA);
+    return false;
+  }
+
+  tron_transaction_contract_t contract = raw_txn->contract[0];
+
+  // TODO: Add switch-cases for more contract types
+  switch (contract.type) {
+    case TRON_TRANSACTION_CONTRACT_TRANSFER_CONTRACT: {
+      if (!transfer_contract_txn(raw_txn, &contract)) {
+        return false;
+      }
+      break;
+    }
+    case TRON_TRANSACTION_CONTRACT_TRIGGER_SMART_CONTRACT: {
+      if (!trigger_smart_contract_txn(raw_txn, &contract)) {
+        return false;
+      }
+      break;
+    }
+    default: {
+      tron_send_error(ERROR_COMMON_ERROR_UNKNOWN_ERROR_TAG,
+                      ERROR_DATA_FLOW_INVALID_REQUEST);
+      return false;
+    }
+  }
+  return true;
+}
