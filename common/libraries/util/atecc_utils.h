245d46c9415c30dac0ebbd41cc098c376a3e75f9
Parnika Cypherock <39666569+VeronicaGupta@users.noreply.github.com>
2024-06-10 14:09:48 +0530

    chore: Init

---
+++ common/libraries/util/atecc_utils.h
@@ -0,0 +1,109 @@
+/**
+ * @file atecc_utils.h
+ * @author  Cypherock X1 Team
+ * @brief Util functions related to atecc signing
+ * @version 0.1
+ * @date 2022-12-16
+ *
+ * @copyright Copyright (c) 2022 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ *
+ */
+
+#ifndef ATECC_UTILS_H
+#define ATECC_UTILS_H
+
+#pragma once
+
+#include "atca_host.h"
+#include "stm32l4xx_it.h"
+
+#define SIGNATURE_SIZE 64
+#define POSTFIX1_SIZE 7
+#define POSTFIX2_SIZE 23
+#define DEVICE_SERIAL_SIZE 32
+#define AUTH_DATA_SERIAL_SIGN_MSG_SIZE                                         \
+  (POSTFIX1_SIZE + POSTFIX2_SIZE + SIGNATURE_SIZE + DEVICE_SERIAL_SIZE)
+#define AUTH_DATA_CHALLENGE_SIGN_MSG_SIZE                                      \
+  (POSTFIX1_SIZE + POSTFIX2_SIZE + SIGNATURE_SIZE)
+
+/**
+ * @brief
+ * @details
+ *
+ * @see
+ * @since v1.0.0
+ *
+ * @note
+ */
+typedef struct auth_data_struct {
+  uint8_t postfix1[POSTFIX1_SIZE], postfix2[POSTFIX2_SIZE],
+      signature[SIGNATURE_SIZE], serial[DEVICE_SERIAL_SIZE];
+} auth_data_t;
+
+typedef enum {
+  slot_0_unused = 0U,
+  slot_1_unused = 1U,
+  slot_2_auth_key = 2U,
+  slot_3_nfc_pair_key = 3U,
+  slot_4_unused = 4U,
+  slot_5_challenge = 5U,
+  slot_6_io_key = 6U,
+  slot_7_unused = 7U,
+  slot_8_serial = 8U,
+  slot_9_unused = 9U,
+  slot_10_unused = 10U,
+  slot_11_unused = 11U,
+  slot_12_unused = 12U,
+  slot_13_unused = 13U,
+  slot_14_unused = 14U,
+  slot_15_unused = 15U
+} atecc_slot_define_t;
+
+typedef struct {
+  uint8_t device_serial[DEVICE_SERIAL_SIZE], retries;
+  ATCA_STATUS status;
+  ATCAIfaceCfg *cfg_atecc608a_iface;
+} atecc_data_t;
+
+/**
+ * @brief
+ * @details
+ *
+ * @see swap_transaction_controller(), device_authentication_controller()
+ *
+ * @param slot
+ * @param data
+ * @param digest
+ * @param postfix
+ * @param atecc_value
+ */
+void helper_get_gendig_hash(atecc_slot_define_t slot,
+                            uint8_t *data,
+                            uint8_t *digest,
+                            uint8_t *postfix,
+                            atecc_data_t *atecc_value);
+
+/**
+ * @brief
+ * @details
+ *
+ * @see swap_transaction_controller(), device_authentication_controller()
+ *
+ * @param param
+ * @param mode
+ * @param priv_key_id
+ * @param data_key_id
+ * @param atecc_value
+ * @return
+ */
+ATCA_STATUS helper_sign_internal_msg(struct atca_sign_internal_in_out *param,
+                                     uint8_t mode,
+                                     uint8_t priv_key_id,
+                                     uint8_t data_key_id,
+                                     atecc_data_t *atecc_value);
+
+auth_data_t atecc_sign(uint8_t *hash);
+
+#endif    // ATECC_UTILS_H
---
+++ common/libraries/util/session_utils.h
@@ -0,0 +1,147 @@
+/**
+ * @file    session_utils.h
+ * @author  Cypherock X1 Team
+ * @brief   Header file containing the session utility functions
+ *          This file declares the functions used to create and manage the
+ *          session, send authentication requests and verify the responses.
+ *
+ * @copyright Copyright (c) 2022 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ */
+
+#ifndef SESSION_UTILS
+#define SESSION_UTILS
+
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "atecc_utils.h"
+#include "base58.h"
+#include "bip32.h"
+#include "nfc.h"
+#include "nist256p1.h"
+#include "rand.h"
+
+#define SESSION_ID_SIZE 32
+#define DEVICE_RANDOM_SIZE 32
+#define SESSION_RANDOM_SIZE 32
+
+/**
+ * @brief Stores the session information
+ * @since v1.0.0
+ */
+#pragma pack(push, 1)
+typedef struct {
+  uint8_t device_random[DEVICE_RANDOM_SIZE];
+  uint8_t session_random[SESSION_RANDOM_SIZE];
+  uint8_t device_id[DEVICE_SERIAL_SIZE];
+  uint8_t session_id[SESSION_ID_SIZE];
+  uint8_t public_key[33];
+  uint32_t session_age;
+} Session;
+#pragma pack(pop)
+
+/**
+ * @brief A generic message structure to send and receive authenticated
+ * messages
+ * @details
+ * For sending an authenticated message to server all the fields have to be
+ * populated. For receiving an authenticated message from the server the
+ * postfix1 and postfix2 fields will be ignored.
+ * @since v1.0.0
+ */
+#pragma pack(push, 1)
+typedef struct {
+  uint16_t message_size;
+  uint8_t *message;
+  uint8_t signature[SIGNATURE_SIZE];
+  uint8_t postfix1[POSTFIX1_SIZE];
+  uint8_t postfix2[POSTFIX2_SIZE];
+} Message;
+#pragma pack(pop)
+
+extern const uint32_t session_key_derv_data[3];
+extern Session session;
+
+/**
+ * @brief Verified the signature of the payload
+ * @param payload The payload to be verified
+ * @param payload_length The length of the payload
+ * @param buffer The buffer to store the signature
+ * @return true if the signature is verified, false otherwise
+ *
+ * @see session_init()
+ * @since v1.0.0
+ */
+bool verify_session_signature(uint8_t *payload,
+                              uint8_t payload_length,
+                              uint8_t *buffer);
+
+/**
+ * @brief Starts the session creation process
+ * @details It generates the device random, derives and stores the device
+ * public key. It also generates the payload to be sent to the server.
+ * @param session_details_data_array The buffer to store the payload to be sent
+ *
+ * @see SESSION_INIT
+ * @since v1.0.0
+ */
+void session_pre_init(uint8_t *session_details_data_array);
+
+/**
+ * @brief Completes the session creation process
+ * @details It verifies the server response and stores the session id and
+ * session random.
+ * @param session_init_details The server response
+ * @param verification_details The buffer to store the details
+ * to be send back to the server to confirm the verification.
+ * @return true if the session is created, false otherwise
+ *
+ * @see SESSION_ESTABLISH
+ * @since v1.0.0
+ */
+bool session_init(uint8_t *session_init_details, uint8_t *verification_details);
+
+/**
+ * @brief Deserializes the an authentication message to Message structure
+ * @param data_array The serialized message
+ * @param msg_size The size of the serialized message
+ * @param msg The Message structure to store the deserialized message
+ *
+ * @since v1.0.0
+ */
+void byte_array_to_session_message(uint8_t *data_array,
+                                   uint16_t msg_size,
+                                   Message *msg);
+
+/**
+ * @brief Serializes the Message structure to an authentication message to
+ * be sent to the server
+ * @param message The Message structure to be serialized
+ * @param data_array The buffer to store the serialized message
+ * @return uint8_t The size of the serialized message
+ *
+ * @since v1.0.0
+ */
+uint8_t session_message_to_byte_array(Message message, uint8_t *data_array);
+
+/**
+ * @brief Generates the payload to be sent to the server.
+ * @details It generates the signature on the payload and appends the
+ * signature to the payload. It further appends the postfix1 and postfix2
+ * for verification on the server side.
+ * @param payload The payload to be sent to the server
+ * @param payload_length The length of the payload
+ * @param signature_details The buffer to store the signature and the postfixes
+ *
+ * @see session_pre_init(), session_init()
+ * @since v1.0.0
+ */
+void append_signature(uint8_t *payload,
+                      uint8_t payload_length,
+                      uint8_t *signature_details);
+
+#endif    // SESSION_UTILS
