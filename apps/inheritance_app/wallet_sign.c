82ef43a4e3b707599f144ccd96dad671a437977d
Vaibhav Sethia <vrockz747@gmail.com>
2024-07-08 20:23:46 +0530

    fix(app): Wallet auth proto types

--- apps/inheritance_app/wallet_sign.c
+++ apps/inheritance_app/wallet_sign.c
@@ -4,16 +4,15 @@
 #include "ui_screens.h"
 
 void inheritance_get_wallet_sign(inheritance_query_t *query) {
-  char *message = query->get_challenge.challenge;
+  char *message = query->wallet_auth.initiate.challenge;
   if (!core_scroll_page("Test Challenge", message, inheritance_send_error)) {
     return;
   }
 
   inheritance_result_t pubkey = INHERITANCE_RESULT_INIT_DEFAULT;
-  pubkey.which_response = INHERITANCE_GET_CHALLENGE_RESPONSE_PUBLIC_KEY_TAG;
-
+  pubkey.which_response = INHERITANCE_WALLET_AUTH_RESPONSE_RESULT_TAG;
   uint8_t bytes[] = {0x11, 0x22, 0x12, 0xfe, 0xff};
-  memcpy(pubkey.get_challenge_response.public_key, bytes, 5);
+  memcpy(pubkey.wallet_auth.result.public_key, bytes, 5);
 
   inheritance_send_result(&pubkey);
 }
