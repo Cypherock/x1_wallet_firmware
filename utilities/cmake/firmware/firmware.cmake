cf2578b3c66c742c27977d96367bec8827819160
TejasvOnly <tejasvonly@gmail.com>
2024-08-15 16:40:15 -0700

    feat: added boilerplate code for inheritace app

--- utilities/cmake/firmware/firmware.cmake
+++ utilities/cmake/firmware/firmware.cmake
@@ -58,7 +58,8 @@ target_include_directories(${EXECUTABLE} PRIVATE
         apps/near_app
         apps/solana_app
         apps/tron_app
-        
+        apps/inheritance_app
+
         src/
         src/menu
         src/wallet
@@ -181,6 +182,7 @@ target_include_directories(${EXECUTABLE} PRIVATE
         $<$<BOOL:UNIT_TESTS_SWITCH>:${PROJECT_SOURCE_DIR}/tests/apps/evm_app>
         $<$<BOOL:UNIT_TESTS_SWITCH>:${PROJECT_SOURCE_DIR}/tests/apps/near_app>
         $<$<BOOL:UNIT_TESTS_SWITCH>:${PROJECT_SOURCE_DIR}/tests/apps/solana_app>
+        $<$<BOOL:UNIT_TESTS_SWITCH>:${PROJECT_SOURCE_DIR}/tests/apps/inheritance_app>
         )
 
 target_compile_options(${EXECUTABLE} PRIVATE
