db08b13d9a832c5b2da0f882d4e88a5a28d50de9
Parnika Gupta <39666569+VeronicaGupta@users.noreply.github.com>
2024-09-23 21:47:33 -0700

    feat: Add pedersen params

---
+++ apps/starknet_app/starknet_padersen.h
@@ -0,0 +1,12 @@
+/**
+ * @file    starknet_crypto.h
+ * @author  Cypherock X1 Team
+ * @brief   Crypto api definitions for Starknet chain
+ * @copyright Copyright (c) 2023 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ */
+#ifndef STARKNET_PADERSEN_H
+#define STARKNET_PADERSEN_H
+
+#endif    // STARKNET_PADERSEN_H
