a310c3a474f56e345ebc8332ef7cc54d16868d0f
Muzaffar Ahmad Bhat <bhatmuzaffar32@gmail.com>
2024-09-26 19:49:49 +0530

    feat: Added boilerplate code for xrp app

---
+++ apps/xrp_app/xrp_context.h
@@ -0,0 +1,38 @@
+/**
+ * @file    xrp_context.h
+ * @author  Cypherock X1 Team
+ * @brief   Header file defining typedefs and MACROS for the XRP app
+ * @copyright Copyright (c) 2024 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ */
+#ifndef XRP_CONTEXT_H
+#define XRP_CONTEXT_H
+
+/*****************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+
+/*****************************************************************************
+ * MACROS AND DEFINES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * TYPEDEFS
+ *****************************************************************************/
+
+// TODO: Populate structure for XRP
+typedef struct {
+} xrp_config_t;
+
+/*****************************************************************************
+ * EXPORTED VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * GLOBAL FUNCTION PROTOTYPES
+ *****************************************************************************/
+
+#endif /* XRP_CONTEXT_H */
---
+++ apps/xrp_app/xrp_priv.h
@@ -0,0 +1,25 @@
+/**
+ * @file    xrp_priv.h
+ * @author  Cypherock X1 Team
+ * @brief   Support for xrp app internal operations
+ *          This file is defined to separate XRP's internal use
+ * functions, flows, common APIs
+ * @copyright Copyright (c) 2024 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ */
+#ifndef XRP_PRIV_H
+#define XRP_PRIV_H
+/*****************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+#include <xrp/core.pb.h>
+#include <stdint.h>
+
+#include "xrp_context.h"
+
+/*****************************************************************************
+ * TYPEDEFS
+ *****************************************************************************/
+
+#endif /* XRP_PRIV_H */
