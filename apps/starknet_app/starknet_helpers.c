faeae41d87bb549f4febd386b1709f516c9a4484
Ayush Srivastava <ayush.alp15@gmail.com>
2024-10-05 17:02:28 -0700

    feat: Remove redundant code and modify function calls to point_add

--- apps/starknet_app/starknet_helpers.c
+++ apps/starknet_app/starknet_helpers.c
@@ -118,10 +118,9 @@ static bool get_stark_child_node(const uint32_t *path,
   return true;
 }
 
-// #include "bignum_internal.h"
 #include "mini-gmp.h"
 #include "mini-gmp-helpers.h"
-// internal-bignum
+// mini-GMP
 bool grind_key(const uint8_t *grind_seed, uint8_t *out) {
     uint8_t key[32] = {0};
     mpz_t strk_limit;
@@ -157,48 +156,6 @@ bool grind_key(const uint8_t *grind_seed, uint8_t *out) {
     LOG_CRITICAL("grind 200 failed");
     return false;
 }
-
-// tiny-bignum
-// static bool grind_key(const uint8_t *grind_seed, uint8_t *out) {
-//   uint8_t key[32] = {0};
-//   struct bn a;
-//   struct bn strk_limit;
-//   struct bn strk_key;
-//   struct bn stark_order;
-//   char str[65] = "";
-
-//   bignum_from_string(
-//       &stark_order,
-//       "0800000000000010FFFFFFFFFFFFFFFFB781126DCAE7B2321E66A241ADC64D2F",
-//       64);
-//   bignum_from_string(
-//       &strk_limit,
-//       "F80000000000020EFFFFFFFFFFFFFFF738A13B4B920E9411AE6DA5F40B0358B1",
-//       64);
-
-//   SHA256_CTX ctx = {0};
-//   for (uint8_t itr = 0; itr < 200; itr++) {
-//     sha256_Init(&ctx);
-//     sha256_Update(&ctx, grind_seed, 32);
-
-//     // copy iteration
-//     sha256_Update(&ctx, &itr, 1);
-//     sha256_Final(&ctx, key);
-
-//     byte_array_to_hex_string(key, 32, str, 65);
-//     bignum_from_string(&strk_key, str, 64);
-//     if (bignum_cmp(&strk_key, &strk_limit) == SMALLER) {
-//       struct bn f_key = {0};
-//       bignum_mod(&strk_key, &stark_order, &f_key);
-//       bignum_to_string(&f_key, str, 64);
-//       hex_string_to_byte_array(str, 64, out);
-//       return true;
-//     }
-//   }
-//   starknet_send_error(ERROR_COMMON_ERROR_UNKNOWN_ERROR_TAG, 0);
-//   LOG_CRITICAL("grind 200 failed");
-//   return false;
-// }
 
 /*****************************************************************************
  * GLOBAL FUNCTIONS
@@ -246,6 +203,7 @@ bool starknet_derive_key_from_seed(const uint8_t *seed_key,
   char hex[100];
 
   // derive node at m/44'/9004'/0'/0/i
+  // send hdKey1.private_key as seed
   if (!get_stark_child_node(
           path, path_length, SECP256K1_NAME, seed_key, 32, &starkChildNode)) {
     // send unknown error; unknown failure reason
@@ -254,12 +212,25 @@ bool starknet_derive_key_from_seed(const uint8_t *seed_key,
     return false;
   }
 
+  printf("\necdsaPrivateKey: ");
+  for (size_t i = 0; i < 32; i++) {
+  printf("%02x", starkChildNode.private_key[i]);
+  }
+  printf("\n");
+
   uint8_t stark_private_key[32];
   stark_point p;
-  uint8_t stark_public_key[32];
+  stark_point_init(&p);
+  // uint8_t stark_public_key[32];
   if (!grind_key(starkChildNode.private_key, stark_private_key)) {
     return false;
   }
+  
+  printf("starkPrivateKey: ");
+  for (size_t i = 0; i < 32; i++) {
+  printf("%02x", stark_private_key[i]);
+  }
+  printf("\n");
 
   char str[100];
   starknet_init();
@@ -282,9 +253,6 @@ bool starknet_derive_key_from_seed(const uint8_t *seed_key,
 
   byte_array_to_hex_string(stark_private_key, 32, hex, 32 * 2 + 1);
   print_hex_array("stark_private_key", stark_private_key, 32);
-
-  // byte_array_to_hex_string(stark_public_key, 33, hex, 33 * 2 + 1);
-  // print_hex_array("stark_public_key", stark_public_key);
 
   memzero(key, 33);
   memcpy(key, stark_private_key, 32);
