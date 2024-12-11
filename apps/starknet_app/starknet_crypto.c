c92707cd4ce3860ee0632c1d867ee0d2ed9ea3d4
Vaibhav Sethia <vrockz747@gmail.com>
2024-12-11 19:46:04 +0530

    feat(app): Add starknet gen pubkey

---
+++ apps/starknet_app/starknet_crypto.c
@@ -0,0 +1,455 @@
+/**
+ * @file    starknet_crypto.c
+ * @author  Cypherock X1 Team
+ * @brief   Crypto specific to Starknet chain
+ * @copyright Copyright (c) 2023 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ *target=_blank>https://mitcc.org/</a>
+ *
+ ******************************************************************************
+ * @attention
+ *
+ * (c) Copyright 2023 by HODL TECH PTE LTD
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * "Commons Clause" License Condition v1.0
+ *
+ * The Software is provided to you by the Licensor under the License,
+ * as defined below, subject to the following condition.
+ *
+ * Without limiting other conditions in the License, the grant of
+ * rights under the License will not include, and the License does not
+ * grant to you, the right to Sell the Software.
+ *
+ * For purposes of the foregoing, "Sell" means practicing any or all
+ * of the rights granted to you under the License to provide to third
+ * parties, for a fee or other consideration (including without
+ * limitation fees for hosting or consulting/ support services related
+ * to the Software), a product or service whose value derives, entirely
+ * or substantially, from the functionality of the Software. Any license
+ * notice or attribution required by the License must also include
+ * this Commons Clause License Condition notice.
+ *
+ * Software: All X1Wallet associated files.
+ * License: MIT
+ * Licensor: HODL TECH PTE LTD
+ *
+ ******************************************************************************
+ */
+
+/*****************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+#include "starknet_crypto.h"
+
+// #include "bignum_internal.h"
+#include <stdio.h>
+
+#include "mini-gmp.h"
+
+/*****************************************************************************
+ * EXTERN VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * PRIVATE MACROS AND DEFINES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * PRIVATE TYPEDEFS
+ *****************************************************************************/
+
+/*****************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * STATIC VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * GLOBAL VARIABLES
+ *****************************************************************************/
+stark_curve *starkCurve;
+stark_pedersen *starkPts;
+
+/*****************************************************************************
+ * STATIC FUNCTIONS
+ *****************************************************************************/
+static void stark_curve_init();
+static void stark_pedersen_init();
+
+/*****************************************************************************
+ * GLOBAL FUNCTIONS
+ *****************************************************************************/
+
+void starknet_init() {
+  stark_curve_init();
+  stark_pedersen_init();
+}
+
+void stark_point_init(stark_point *p) {
+  mpz_init(p->x);
+  mpz_init(p->y);
+}
+
+void stark_point_clear(stark_point *p) {
+  mpz_clear(p->x);
+  mpz_clear(p->y);
+}
+
+void stark_pedersen_clear(stark_pedersen *pedersen) {
+  for (int i = 0; i < 5; i++) {
+    stark_point_clear(&pedersen->P[i]);
+  }
+}
+
+void stark_curve_init() {
+  static stark_curve stark256;
+  // char str[STARK_BN_LEN] = {0};
+
+  /* stark_curve_params ref:
+  https://github.com/xJonathanLEI/starknet-rs/blob/f31e426a65225b9830bbf3c148f7ea05bf9dc257/starknet-curve/src/curve_params.rs
+
+  struct bn prime;         // prime order of the finite field
+  stark_point G;           // initial curve point
+  struct bn order;         // order of G
+  struct bn order_half;    // order of G divided by 2
+  struct bn a;             // coefficient 'a' of the elliptic curve OR alpha
+  struct bn b;             // coefficient 'b' of the elliptic curve OR beta
+  */
+
+  // Initialize mpz_t variables in stark256
+  mpz_init(stark256.prime);
+  mpz_init(stark256.G.x);
+  mpz_init(stark256.G.y);
+  mpz_init(stark256.order);
+  mpz_init(stark256.order_half);
+  mpz_init(stark256.a);
+  mpz_init(stark256.b);
+
+  // Prime
+  mpz_set_str(
+      stark256.prime,
+      "0800000000000011000000000000000000000000000000000000000000000001",
+      16);
+
+  // Generator_point x
+  mpz_set_str(
+      stark256.G.x,
+      "01EF15C18599971B7BECED415A40F0C7DEACFD9B0D1819E03D723D8BC943CFCA",
+      16);
+
+  // Generator_point y
+  mpz_set_str(
+      stark256.G.y,
+      "005668060AA49730B7BE4801DF46EC62DE53ECD11ABE43A32873000C36E8DC1F",
+      16);
+
+  // Order
+  mpz_set_str(
+      stark256.order,
+      "0800000000000010ffffffffffffffffb781126dcae7b2321e66a241adc64d2f",
+      16);
+
+  // Order half
+  mpz_set_str(
+      stark256.order_half,
+      "04000000000000087fffffffffffffffdbc08936e573d9190f335120d6e32697",
+      16);
+
+  // Alpha
+  mpz_set_str(
+      stark256.a,
+      "0000000000000000000000000000000000000000000000000000000000000001",
+      16);
+
+  // Beta
+  mpz_set_str(
+      stark256.b,
+      "06f21413efbe40de150e596d72f7a8c5609ad26c15c915c1f4cdfcb99cee9e89",
+      16);
+
+  starkCurve = &stark256;
+  // print_stark_curve();
+}
+
+static void stark_pedersen_init() {
+  // Ref: https://docs.starkware.co/starkex/crypto/pedersen-hash-function.html
+
+  static stark_pedersen pedersen;
+  // Initialize all mpz_t variables in the pedersen structure
+  for (int i = 0; i < 5; i++) {
+    mpz_init(pedersen.P[i].x);
+    mpz_init(pedersen.P[i].y);
+  }
+
+  // Shift_point x
+  mpz_set_str(
+      pedersen.P[0].x,
+      "049EE3EBA8C1600700EE1B87EB599F16716B0B1022947733551FDE4050CA6804",
+      16);
+
+  // Shift_point y
+  mpz_set_str(
+      pedersen.P[0].y,
+      "03CA0CFE4B3BC6DDF346D49D06EA0ED34E621062C0E056C1D0405D266E10268A",
+      16);
+
+  // Pedersen_point_1 x
+  mpz_set_str(
+      pedersen.P[1].x,
+      "0234287DCBAFFE7F969C748655FCA9E58FA8120B6D56EB0C1080D17957EBE47B",
+      16);
+
+  // Pedersen_point_1 y
+  mpz_set_str(
+      pedersen.P[1].y,
+      "03B056F100F96FB21E889527D41F4E39940135DD7A6C94CC6ED0268EE89E5615",
+      16);
+
+  // Pedersen_point_2 x
+  mpz_set_str(
+      pedersen.P[2].x,
+      "04FA56F376C83DB33F9DAB2656558F3399099EC1DE5E3018B7A6932DBA8AA378",
+      16);
+
+  // Pedersen_point_2 y
+  mpz_set_str(
+      pedersen.P[2].y,
+      "03FA0984C931C9E38113E0C0E47E4401562761F92A7A23B45168F4E80FF5B54D",
+      16);
+
+  // Pedersen_point_3 x
+  mpz_set_str(
+      pedersen.P[3].x,
+      "04BA4CC166BE8DEC764910F75B45F74B40C690C74709E90F3AA372F0BD2D6997",
+      16);
+
+  // Pedersen_point_3 y
+  mpz_set_str(pedersen.P[3].y,
+              "040301CF5C1751F4B971E46C4EDE85FCAC5C59A5CE5AE7C48151F27B24B219C",
+              16);
+
+  // Pedersen_point_4 x
+  mpz_set_str(
+      pedersen.P[4].x,
+      "054302DCB0E6CC1C6E44CCA8F61A63BB2CA65048D53FB325D36FF12C49A58202",
+      16);
+
+  // Pedersen_point_4 y
+  mpz_set_str(
+      pedersen.P[4].y,
+      "01B77B3E37D13504B348046268D8AE25CE98AD783C25561A879DCC77E99C2426",
+      16);
+
+  starkPts = &pedersen;
+}
+
+// Set cp2 = cp1
+void stark_point_copy(const stark_point *cp1, stark_point *cp2) {
+  stark_point_init(cp2);
+
+  mpz_set(cp2->x, cp1->x);
+  mpz_set(cp2->y, cp1->y);
+}
+
+void stark_point_add(const stark_curve *curve,
+                     const stark_point *cp1,
+                     stark_point *cp2) {
+  mpz_t lambda, inv, xr, yr;
+
+  mpz_init(lambda);
+  mpz_init(inv);
+  mpz_init(xr);
+  mpz_init(yr);
+
+  if (stark_point_is_infinity(cp1)) {
+    return;
+  }
+  if (stark_point_is_infinity(cp2)) {
+    stark_point_copy(cp1, cp2);
+    return;
+  }
+  if (stark_point_is_equal(cp1, cp2)) {
+    stark_point_double(curve, cp2);
+    return;
+  }
+  if (stark_point_is_negative_of(cp1, cp2)) {
+    stark_point_set_infinity(cp2);
+    return;
+  }
+
+  // inv = (cp2->x - cp1->x) mod prime
+  mpz_sub(inv, cp2->x, cp1->x);
+  mpz_mod(inv, inv, curve->prime);
+
+  // inv = inv^-1 mod prime
+  mpz_invert(inv, inv, curve->prime);
+
+  // lambda = (cp2->y - cp1->y) mod prime
+  mpz_sub(lambda, cp2->y, cp1->y);
+  mpz_mod(lambda, lambda, curve->prime);
+
+  // lambda = lambda * inv mod prime
+  mpz_mul(lambda, lambda, inv);
+  mpz_mod(lambda, lambda, curve->prime);
+
+  // xr = lambda^2 - cp1->x - cp2->x mod prime
+  mpz_mul(xr, lambda, lambda);
+  mpz_sub(xr, xr, cp1->x);
+  mpz_sub(xr, xr, cp2->x);
+  mpz_mod(xr, xr, curve->prime);
+
+  // yr = lambda * (cp1->x - xr) - cp1->y mod prime
+  mpz_sub(yr, cp1->x, xr);
+  mpz_mul(yr, yr, lambda);
+  mpz_sub(yr, yr, cp1->y);
+  mpz_mod(yr, yr, curve->prime);
+
+  // Set cp2 to the result
+  mpz_set(cp2->x, xr);
+  mpz_set(cp2->y, yr);
+
+  mpz_clear(lambda);
+  mpz_clear(inv);
+  mpz_clear(xr);
+  mpz_clear(yr);
+}
+
+void stark_point_double(const stark_curve *curve, stark_point *cp) {
+  // Ref:
+  // https://github.com/starkware-libs/starkex-for-spot-trading/blob/607f0b4ce507e1d95cd018d206a2797f6ba4aab4/src/starkware/crypto/starkware/crypto/signature/math_utils.py
+  if (mpz_cmp_ui(cp->y, 0) == 0) {
+    return;
+  }
+
+  mpz_t lambda, xr, yr, inv;
+  mpz_init(lambda);
+  mpz_init(xr);
+  mpz_init(yr);
+  mpz_init(inv);
+
+  // lambda = (3 * cp->x^2 + curve->a) * (2 * cp->y)^-1 mod prime
+  mpz_mul(lambda, cp->x, cp->x);
+  mpz_mul_ui(lambda, lambda, 3);
+  mpz_add(lambda, lambda, curve->a);
+  mpz_mul_ui(inv, cp->y, 2);    // using inv to store 2 * y
+  mpz_invert(inv, inv, curve->prime);
+  mpz_mul(lambda, lambda, inv);
+  mpz_mod(lambda, lambda, curve->prime);
+
+  // xr = lambda^2 - 2 * cp->x mod prime
+  mpz_mul(xr, lambda, lambda);
+  mpz_submul_ui(xr, cp->x, 2);
+  mpz_mod(xr, xr, curve->prime);
+
+  // yr = lambda * (cp->x - xr) - cp->y mod prime
+  mpz_sub(yr, cp->x, xr);
+  mpz_mul(yr, yr, lambda);
+  mpz_sub(yr, yr, cp->y);
+  mpz_mod(yr, yr, curve->prime);
+
+  mpz_set(cp->x, xr);
+  mpz_set(cp->y, yr);
+
+  mpz_clear(lambda);
+  mpz_clear(xr);
+  mpz_clear(yr);
+  mpz_clear(inv);
+}
+
+// set point to internal representation of point at infinity
+void stark_point_set_infinity(stark_point *p) {
+  mpz_set_ui(p->x, 0);
+  mpz_set_ui(p->y, 0);
+}
+
+// return true iff p represent point at infinity
+// both coords are zero in internal representation
+int stark_point_is_infinity(const stark_point *p) {
+  return mpz_cmp_ui(p->x, 0) == 0 && mpz_cmp_ui(p->y, 0) == 0;
+}
+
+// return true iff both points are equal
+int stark_point_is_equal(const stark_point *p, const stark_point *q) {
+  return (mpz_cmp(p->x, q->x) == 0) && (mpz_cmp(p->y, q->y) == 0);
+}
+
+// returns true iff p == -q
+// expects p and q be valid points on curve other than point at infinity
+int stark_point_is_negative_of(const stark_point *p, const stark_point *q) {
+  // if P == (x, y), then -P would be (x, -y) on this curve
+  if (mpz_cmp(p->x, q->x) != 0) {
+    return 0;
+  }
+
+  // we shouldn't hit this for a valid point
+  if (mpz_cmp_ui(p->y, 0) == 0) {
+    return 0;
+  }
+
+  return mpz_cmp(p->y, q->y) != 0;
+}
+
+void stark_point_multiply(const stark_curve *curve,
+                          const mpz_t k,
+                          const stark_point *p,
+                          stark_point *res) {
+  // Ref: https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+
+  stark_point temp;
+  stark_point R;
+  stark_point_init(&temp);
+  stark_point_init(&R);
+  stark_point_set_infinity(&R);    // Initialize R to the point at infinity
+  stark_point_copy(p, &temp);      // Copy the input point p to temp
+
+  // Iterate over each bit of k from the least significant to the most
+  // significant
+  for (int i = 0; i < 256; i++) {
+    // If the i-th bit of k is set, add temp to the result R
+    if (mpz_tstbit(k, i)) {
+      stark_point_add(curve, &temp, &R);
+    }
+
+    // Double the current point temp
+    stark_point_double(curve, &temp);
+  }
+
+  // Copy the result R to the output parameter res
+  stark_point_copy(&R, res);
+
+  stark_point_clear(&temp);
+  stark_point_clear(&R);
+}
+
+int bn_bit_length(const mpz_t k) {
+  if (mpz_cmp_ui(k, 0) == 0) {
+    return 0;
+  }
+  return mpz_sizeinbase(k, 2);
+}
+
+int bn_is_bit_set(const mpz_t k, int bit_idx) {
+  return mpz_tstbit(k, bit_idx);
+}
--- apps/starknet_app/starknet_pedersen.c
+++ apps/starknet_app/starknet_pedersen.c
@@ -1,7 +1,7 @@
 /**
- * @file    core_flow_init.c
+ * @file    starknet_padersen.c
  * @author  Cypherock X1 Team
- * @brief
+ * @brief   Utilities specific to Starknet chains
  * @copyright Copyright (c) 2023 HODL TECH PTE LTD
  * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
  *target=_blank>https://mitcc.org/</a>
@@ -59,33 +59,16 @@
 /*****************************************************************************
  * INCLUDES
  *****************************************************************************/
-#include "core_flow_init.h"
 
-#include "app_registry.h"
-#include "application_startup.h"
-#include "arbitrum_app.h"
-#include "avalanche_app.h"
-#include "bsc_app.h"
-#include "btc_app.h"
-#include "btc_main.h"
-#include "dash_app.h"
-#include "doge_app.h"
-#include "eth_app.h"
-#include "evm_main.h"
-#include "fantom_app.h"
-#include "inheritance_main.h"
-#include "ltc_app.h"
-#include "main_menu.h"
-#include "manager_app.h"
-#include "near_main.h"
-#include "onboarding.h"
-#include "optimism_app.h"
-#include "polygon_app.h"
-#include "restricted_app.h"
-#include "solana_main.h"
-#include "starknet_main.h"
-#include "tron_main.h"
-#include "xrp_main.h"
+#include <error.pb.h>
+#include <stdint.h>
+
+#include "coin_utils.h"
+#include "mini-gmp-helpers.h"
+#include "starknet_api.h"
+#include "starknet_context.h"
+#include "starknet_crypto.h"
+#include "starknet_helpers.h"
 
 /*****************************************************************************
  * EXTERN VARIABLES
@@ -94,95 +77,136 @@
 /*****************************************************************************
  * PRIVATE MACROS AND DEFINES
  *****************************************************************************/
-#define CORE_ENGINE_BUFFER_SIZE 10
 
 /*****************************************************************************
  * PRIVATE TYPEDEFS
  *****************************************************************************/
 
 /*****************************************************************************
+ * STATIC FUNCTION PROTOTYPES
+ *****************************************************************************/
+static void process_single_element(mpz_t element,
+                                   stark_point *p1,
+                                   stark_point *p2,
+                                   stark_point *result);
+
+/*****************************************************************************
  * STATIC VARIABLES
  *****************************************************************************/
-flow_step_t *core_step_buffer[CORE_ENGINE_BUFFER_SIZE] = {0};
-engine_ctx_t core_step_engine_ctx = {
-    .array = &core_step_buffer[0],
-    .current_index = 0,
-    .max_capacity = sizeof(core_step_buffer) / sizeof(core_step_buffer[0]),
-    .num_of_elements = 0,
-    .size_of_element = sizeof(core_step_buffer[0])};
 
 /*****************************************************************************
  * GLOBAL VARIABLES
  *****************************************************************************/
 
 /*****************************************************************************
- * STATIC FUNCTION PROTOTYPES
- *****************************************************************************/
-
-/*****************************************************************************
  * STATIC FUNCTIONS
  *****************************************************************************/
+bool pederson_hash(uint8_t *x, uint8_t *y, uint8_t size, uint8_t *hash) {
+  ASSERT(NULL != x);
+  ASSERT(NULL != y);
+  ASSERT(0 < size);
+
+  // Convert to bn
+  mpz_t a, b, result;
+  mpz_init(a);
+  mpz_init(b);
+  mpz_init(result);
+
+  mpz_import(a, size, 1, 1, 1, 0, x);    // Convert x to mpz_t a
+  mpz_import(b, size, 1, 1, 1, 0, y);    // Convert y to mpz_t b
+
+  // Get shift point
+  stark_point HASH_SHIFT_POINT, P_1, P_2, P_3, P_4;
+  stark_point_copy(&starkPts->P[0], &HASH_SHIFT_POINT);
+  stark_point_copy(&starkPts->P[1], &P_1);
+  stark_point_copy(&starkPts->P[2], &P_2);
+  stark_point_copy(&starkPts->P[3], &P_3);
+  stark_point_copy(&starkPts->P[4], &P_4);
+
+  // Compute the hash using the Starkware Pedersen hash definition
+  stark_point x_part, y_part, hash_point;
+  stark_point_init(&x_part);
+  stark_point_init(&y_part);
+  stark_point_init(&hash_point);
+
+  process_single_element(a, &P_1, &P_2, &x_part);
+  process_single_element(b, &P_3, &P_4, &y_part);
+
+  stark_point_add(starkCurve, &HASH_SHIFT_POINT, &x_part);
+  stark_point_add(starkCurve, &x_part, &y_part);
+  stark_point_copy(&y_part, &hash_point);
+
+  memzero(hash, 32);
+  mpz_to_byte_array(hash_point.x, hash, 32);
+
+  return true;
+}
+
+void process_single_element(mpz_t element,
+                            stark_point *p1,
+                            stark_point *p2,
+                            stark_point *result) {
+  ASSERT(mpz_cmp(element, starkCurve->prime) < 0);
+
+  mpz_t low_part, high_nibble;
+  mpz_init(low_part);
+  mpz_init(high_nibble);
+
+  // Extract the low 248 bits and high bits from the element
+  mpz_t mask;
+  mpz_init(mask);
+  // Set mask to (1 << 248) - 1
+  mpz_ui_pow_ui(mask, 2, 248);    // mask = 2^248
+  mpz_sub_ui(mask, mask, 1);      // mask = 2^248 - 1
+  // Extract the low 248 bits and high bits from the element
+  mpz_and(low_part, element, mask);
+  mpz_fdiv_q_2exp(high_nibble, element, LOW_PART_BITS);
+
+  stark_point res1, res2;
+  stark_point_init(&res1);
+  stark_point_init(&res2);
+
+  stark_point_multiply(starkCurve, low_part, p1, &res1);    // low_part * p1
+  stark_point_multiply(
+      starkCurve, high_nibble, p2, &res2);    // high_nibble * p2
+  stark_point_add(starkCurve, &res1, &res2);
+
+  stark_point_copy(&res2, result);
+
+  // clear mpz vars
+  mpz_clear(low_part);
+  mpz_clear(high_nibble);
+  mpz_clear(mask);
+}
 
 /*****************************************************************************
  * GLOBAL FUNCTIONS
  *****************************************************************************/
-engine_ctx_t *get_core_flow_ctx(void) {
-  engine_reset_flow(&core_step_engine_ctx);
+void starknet_uli_to_bn_byte_array(const unsigned long int ui,
+                                   uint8_t *bn_array) {
+  mpz_t bn;
+  mpz_init(bn);
+  mpz_set_ui(bn, ui);
 
-  const manager_onboarding_step_t step = onboarding_get_last_step();
-  /// Check if onboarding is complete or not
-  if (MANAGER_ONBOARDING_STEP_COMPLETE != step) {
-    // reset partial-onboarding if auth flag is reset (which can happen via
-    // secure-bootloader). Refer PRF-7078
-    if (MANAGER_ONBOARDING_STEP_VIRGIN_DEVICE < step &&
-        DEVICE_NOT_AUTHENTICATED == get_auth_state()) {
-      // bypass onboarding_set_step_done as we want to force reset
-      save_onboarding_step(MANAGER_ONBOARDING_STEP_VIRGIN_DEVICE);
-    }
-
-    // Skip onbaording for infield devices with pairing and/or wallets count is
-    // greater than zero
-    if ((get_wallet_count() > 0) || (get_keystore_used_count() > 0)) {
-      onboarding_set_step_done(MANAGER_ONBOARDING_STEP_COMPLETE);
-    } else {
-      engine_add_next_flow_step(&core_step_engine_ctx, onboarding_get_step());
-      return &core_step_engine_ctx;
-    }
-  }
-
-  // Check if device needs to go to restricted state or not
-  if (DEVICE_AUTHENTICATED != get_auth_state()) {
-    engine_add_next_flow_step(&core_step_engine_ctx, restricted_app_get_step());
-    return &core_step_engine_ctx;
-  }
-
-  if (MANAGER_ONBOARDING_STEP_COMPLETE == get_onboarding_step() &&
-      DEVICE_AUTHENTICATED == get_auth_state()) {
-    check_invalid_wallets();
-  }
-
-  // Finally enable all flows from the user
-  engine_add_next_flow_step(&core_step_engine_ctx, main_menu_get_step());
-  return &core_step_engine_ctx;
+  memzero(bn_array, STARKNET_BIGNUM_SIZE);
+  mpz_to_byte_array(bn, bn_array, STARKNET_BIGNUM_SIZE);
 }
 
-void core_init_app_registry() {
-  registry_add_app(get_manager_app_desc());
-  registry_add_app(get_btc_app_desc());
-  registry_add_app(get_ltc_app_desc());
-  registry_add_app(get_doge_app_desc());
-  registry_add_app(get_dash_app_desc());
-  registry_add_app(get_eth_app_desc());
-  registry_add_app(get_near_app_desc());
-  registry_add_app(get_polygon_app_desc());
-  registry_add_app(get_solana_app_desc());
-  registry_add_app(get_bsc_app_desc());
-  registry_add_app(get_fantom_app_desc());
-  registry_add_app(get_avalanche_app_desc());
-  registry_add_app(get_optimism_app_desc());
-  registry_add_app(get_arbitrum_app_desc());
-  registry_add_app(get_tron_app_desc());
-  registry_add_app(get_inheritance_app_desc());
-  registry_add_app(get_xrp_app_desc());
-  registry_add_app(get_starknet_app_desc());
+void compute_hash_on_elements(uint8_t data[][STARKNET_BIGNUM_SIZE],
+                              uint8_t num_elem,
+                              uint8_t *hash) {
+  uint8_t result[STARKNET_BIGNUM_SIZE];
+  memzero(result, STARKNET_BIGNUM_SIZE);
+
+  for (uint8_t index = 0; index < num_elem; index++) {
+    pederson_hash(result, data[index], STARKNET_BIGNUM_SIZE, result);
+  }
+
+  uint8_t num_elem_bn[32];
+  starknet_uli_to_bn_byte_array(num_elem, num_elem_bn);
+
+  pederson_hash(result, num_elem_bn, STARKNET_BIGNUM_SIZE, result);
+
+  memcpy(hash, result, STARKNET_BIGNUM_SIZE);
+  return;
 }
