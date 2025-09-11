855f83057bdb5b7496941772564cc1c78790913f
Tunnel_Rat <reachashish2004@gmail.com>
2025-09-12 01:15:09 +0530

    feat: Add hyperliquid.c and hyperliquid.h to evm_family

---
+++ apps/evm_family/hyperliquid/hyperliquid_app.h
@@ -0,0 +1,44 @@
+/**
+ * @file    hyperliquid_app.h
+ * @author  Cypherock X1 Team
+ * @brief   Hyperliquid application configuration
+ * @copyright Copyright (c) 2023 HODL TECH PTE LTD
+ * <br/> You may obtain a copy of license at <a href="https://mitcc.org/"
+ * target=_blank>https://mitcc.org/</a>
+ */
+#ifndef HYPERLIQUID_APP_H
+#define HYPERLIQUID_APP_H
+
+/*****************************************************************************
+ * INCLUDES
+ *****************************************************************************/
+
+#include "app_registry.h"
+#include "evm_context.h"
+/*****************************************************************************
+ * MACROS AND DEFINES
+ *****************************************************************************/
+
+/// Number of entries in whitelisted contracts list
+#define HYPERLIQUID_WHITELISTED_CONTRACTS_COUNT 0
+
+/*****************************************************************************
+ * TYPEDEFS
+ *****************************************************************************/
+
+/*****************************************************************************
+ * EXPORTED VARIABLES
+ *****************************************************************************/
+
+/*****************************************************************************
+ * GLOBAL FUNCTION PROTOTYPES
+ *****************************************************************************/
+
+/**
+ * @brief Returns the config for Hyperliquid chin app descriptor
+ *
+ * @return A const reference to cy_app_desc_t
+ */
+const cy_app_desc_t *get_hyperliquid_app_desc();
+
+#endif    // HYPERLIQUID_APP_H
