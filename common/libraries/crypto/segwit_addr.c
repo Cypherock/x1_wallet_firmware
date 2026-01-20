402c6d4c77ff6ffa67c286b08ea58c7371f4f5e9
Prashant Rahul <prashantrahul141@protonmail.com>
2026-01-20 17:25:07 +0530

    fix(lib): Increase bech32_encode max len
    Cardano addresses go far more than the traditional limit of 90 in bech32
    encoding
    
    Signed-off-by: Prashant Rahul <prashantrahul141@protonmail.com>

--- common/libraries/crypto/segwit_addr.c
+++ common/libraries/crypto/segwit_addr.c
@@ -60,7 +60,7 @@ int bech32_encode(char *output, const char *hrp, const uint8_t *data, size_t dat
         chk = bech32_polymod_step(chk) ^ (ch >> 5);
         ++i;
     }
-    if (i + 7 + data_len > 90) return 0;
+    if (i + 7 + data_len > 150) return 0;
     chk = bech32_polymod_step(chk);
     while (*hrp != 0) {
         chk = bech32_polymod_step(chk) ^ (*hrp & 0x1f);
